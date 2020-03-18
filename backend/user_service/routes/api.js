377f8e725d7dd119204e2d4f4ce67d4b105ae316
Sanath Swaroop <sanath15swaroop@gmail.com>
2020-03-18 12:50:58 +0530

    removed trackingcontroller from user_services

--- backend/user_service/routes/api.js
+++ backend/user_service/routes/api.js
@@ -2,12 +2,10 @@ var express = require("express");
 var authRouter = require("./auth");
 var inventoryQueryRouter = require("./inventory")
 var shippingQueryRouter = require("./shipping")
-var trackQueryRouter = require("./track.js")
 var app = express();
 
 app.use("/auth/", authRouter);
 app.use("/inventory",inventoryQueryRouter);
 app.use("/shipping",shippingQueryRouter);
-app.use("/track", trackQueryRouter);
 
 module.exports = app;
