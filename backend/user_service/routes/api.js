c3cebe8df22040741849cc880456aad961af7c81
Sanath Swaroop <sanath15swaroop@gmail.com>
2020-03-18 09:42:24 +0530

    refactored code Track and Trace API

--- backend/user_service/routes/api.js
+++ backend/user_service/routes/api.js
@@ -2,10 +2,12 @@ var express = require("express");
 var authRouter = require("./auth");
 var inventoryQueryRouter = require("./inventory")
 var shippingQueryRouter = require("./shipping")
+var trackQueryRouter = require("./track.js")
 var app = express();
 
 app.use("/auth/", authRouter);
 app.use("/inventory",inventoryQueryRouter);
 app.use("/shipping",shippingQueryRouter);
+app.use("/track", trackQueryRouter);
 
 module.exports = app;
