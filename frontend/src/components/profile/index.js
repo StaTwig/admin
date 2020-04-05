b6d782240bc7bac4b9c4353ba58a0d062dbf0881
Sanath Swaroop <sanath15swaroop@gmail.com>
2020-04-05 21:19:45 +0530

    Profile picture update

--- frontend/src/components/profile/index.js
+++ frontend/src/components/profile/index.js
@@ -1,47 +1,65 @@
-import React from "react";
+import React, { Component } from 'react';
 import ProfilePic from "../../assets/brands/user-image/Image73@2x.png";
 import Pen from "../../assets/icons/pen.svg";
 import './style.scss';
 import { SERVER_URL } from '../../config';
 const axios = require("axios");
 
-class Profile extends React.Component {
-  
+class Profile extends React.Component{
+// const Profile = props => {
+  // const [file, setFile] = useState('')
+  // const [profile, setProfile] = useState('')
+  // const upload = useRef(null);
   constructor(props) {
     super(props);
     this.state ={
-        file: null,
+        selectedFile: null,
         profile: null
     };
     this.onChange = this.onChange.bind(this);
 }
 componentDidMount() {
-  axios.get(`${SERVER_URL}/usermanagement/auth/userInfo`)
+  axios.get(`${SERVER_URL}/usermanagement/api/auth/userInfo`)
     .then(res => {
-      const profile = res.data;
-      this.setState({ profile });
+      const prof = res.data;
+      this.setState({profile : prof})
+      console.log(prof)
+      console.log(this.state.profile)
     })
+    
 }
-onChange(e) {
-    this.setState({file:e.target.files[0]});    
+
+// useEffect(() => {
+//   axios.get(`${SERVER_URL}/auth/userInfo`)
+//     .then(res => {
+//       const profile = res.data;
+//       setProfile({ profile });
+//     })
+//  }, [])
+
+onChange(e)  {
+  this.setState({ selectedFile: event.target.files[0] }) 
     e.preventDefault();
-    console.log(this.state.file)
+    console.log(this.state.selectedFile)
     const formData = new FormData();
-    formData.append('profile',this.state.file);
+    formData.append('profile',this.state.selectedFile);
     const config = {
         headers: {
             'content-type': 'multipart/form-data'
         }
     };
-    axios.post(`${SERVER_URL}/usermanagement/auth/upload`,formData,config)
+    axios.post(`${SERVER_URL}/usermanagement/api/auth/upload`,formData,config)
         .then((response) => {
             alert("The file is successfully uploaded" + response);
         }).catch((error) => {
           alert(error);
     });
+    state = { selectedFile: null }
 }
+
+
+
 render(){
-  console.log(this.state.profile)
   return (
     <div className="profile">
       <h1 className="breadcrumb">Profile</h1>
@@ -54,13 +72,13 @@ render(){
             {this.state.profile ? <img src={this.state.profile.data.profile_picture} alt="John Name" className="rounded rounded-circle" />: ''}
            
           </div>
-          
-          <input id="profile" type="file" ref={(ref) => this.upload = ref} onChange= {this.onChange} style={{ display: 'none' }} />
+          <input id="profile" onChange={this.onChange} type="file" ref={(ref) => this.upload = ref} style={{ display: 'none' }} />
+    
           <button type="button" onClick={(e) => this.upload.click() } className="btn btn-outline-info">Change Photo</button>
          
           </div>
           <div className="col-8 mt-5">
-          {this.state.profile ? <h3>{this.state.profile.data.name}</h3> : 'John Doe'}
+          {this.state.profile ? <h3>{this.state.profile.data.name}</h3> : 'Name'}
           <div className="row">
            <ul>
            <li>Role</li>
@@ -94,7 +112,7 @@ render(){
     </div>
    
   );
-}
+  }
 };
 
 export default Profile;
