9219e446327694b764d014343f9705e861774cf1
Sanath Swaroop <sanath15swaroop@gmail.com>
2020-04-08 20:35:41 +0530

    added cors for usermanagement

--- backend/user_service/routes/api.js
+++ backend/user_service/routes/api.js
@@ -1,7 +1,8 @@
 var express = require("express");
 var authRouter = require("./auth");
 var app = express();
-
+var cors = require('cors')
+app.use(cors())
 app.use("/auth/", authRouter);
 
 module.exports = app;
