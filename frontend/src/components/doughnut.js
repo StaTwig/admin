2b20a42d464016a269db65051665c86f265c9e48
theone3nu <mytu3nu@gmail.com>
2020-07-08 00:43:08 +0530

    removed errors

--- frontend/src/components/doughnut.js
+++ frontend/src/components/doughnut.js
@@ -5,57 +5,59 @@ import { getInventories} from '../actions/inventoryActions';
 
 const ChartsPage = (props) => {
 
-const ChartsPage = props => {
-  useEffect(() => {
-    async function fetchData() {
-      getInventories();
-    }
-    fetchData();
-  }, []);
-  const inventoriesKeys = Object.keys(props.inventoriesCount);
-  const filteredInventoriesKeys = inventoriesKeys.filter(
-    inventory => inventory !== 'tot_qty' && inventory !== 'tot_inv',
-  );
-  const labels = filteredInventoriesKeys.filter(
-    inventoryKey => props.inventoriesCount[inventoryKey] !== 0,
-  );
-  const [dataDoughnut] = useState({
-    labels: labels,
-    datasets: [
-      {
-        data: [
-          props.inventoriesCount.bOPV,
-          props.inventoriesCount.MMR,
-          props.inventoriesCount.PVC,
-          props.inventoriesCount.BCG,
-          props.inventoriesCount.RV,
-          props.inventoriesCount.HepB,
-        ],
-        backgroundColor: [
-          '#ffbcc4',
-          '#c1e3f2',
-          '#ffc18c',
-          '#ffef83',
-          '#d4e7ff',
-          '#e0b0ff',
-        ],
-        hoverBackgroundColor: [
-          '#ffbcc4',
-          '#c1e3f2',
-          '#ffc18c',
-          '#ffef83',
-          '#d4e7ff',
-          '#e0b0ff',
-        ],
-      },
-    ],
-  });
+  const ChartsPage = props => {
+    useEffect(() => {
+      async function fetchData() {
+        getInventories();
+      }
 
-  return (
-    <MDBContainer>
-      <Doughnut data={dataDoughnut} />
-    </MDBContainer>
-  );
-};
+      fetchData();
+    }, []);
+    const inventoriesKeys = Object.keys(props.inventoriesCount);
+    const filteredInventoriesKeys = inventoriesKeys.filter(
+      inventory => inventory !== 'tot_qty' && inventory !== 'tot_inv',
+    );
+    const labels = filteredInventoriesKeys.filter(
+      inventoryKey => props.inventoriesCount[inventoryKey] !== 0,
+    );
+    const [dataDoughnut] = useState({
+      labels: labels,
+      datasets: [
+        {
+          data: [
+            props.inventoriesCount.bOPV,
+            props.inventoriesCount.MMR,
+            props.inventoriesCount.PVC,
+            props.inventoriesCount.BCG,
+            props.inventoriesCount.RV,
+            props.inventoriesCount.HepB,
+          ],
+          backgroundColor: [
+            '#ffbcc4',
+            '#c1e3f2',
+            '#ffc18c',
+            '#ffef83',
+            '#d4e7ff',
+            '#e0b0ff',
+          ],
+          hoverBackgroundColor: [
+            '#ffbcc4',
+            '#c1e3f2',
+            '#ffc18c',
+            '#ffef83',
+            '#d4e7ff',
+            '#e0b0ff',
+          ],
+        },
+      ],
+    });
+
+    return (
+      <MDBContainer>
+        <Doughnut data={dataDoughnut}/>
+      </MDBContainer>
+    );
+  };
+}
 
 export default ChartsPage;
