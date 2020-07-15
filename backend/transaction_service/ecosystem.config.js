fdabb4bd2532f6b5ee75dfc329c634ed7bfb4af8
Ashwini Shankar Patankar <ashwinispatankar@gmail.com>
2020-07-14 20:25:56 -0400

    [DevOps] Renamed some files for more proper organisation

--- backend/transaction_service/ecosystem.config.js
+++ backend/transaction_service/ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps : [{
     name: 'Transaction Service',
-    script: 'index.js',
+    script: 'transaction_server.js',
 
     // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
     args: 'one two',
