02371e9c71795820721d49428c29c1a9f0627b3a
Ashwini Shankar Patankar <ashwinispatankar@gmail.com>
2020-07-14 20:34:10 -0400

    [DevOps] Fixed the missing file

--- backend/notification_service/ecosystem.config.js
+++ backend/notification_service/ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps : [{
     name: 'Notification Service',
-    script: 'index.js',
+    script: 'notification_server.js',
 
     // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
     args: 'one two',
