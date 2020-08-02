72a54174eb4d95125eb6a0e2d1531ee94524056c
theone3nu <mytu3nu@gmail.com>
2020-08-03 01:53:09 +0530

    product service

--- backend/products_service/models/ProductNamesModel.js
+++ backend/products_service/models/ProductNamesModel.js
@@ -1,7 +1,13 @@
 const mongoose = require("mongoose");
 
 const ProductNamesSchema = new mongoose.Schema({
-	productName: {type: String, required: true},
+	productName: {type: String, required: true, unique: true},
+	manufacturer: {type: String, required: true},
+	productCategory: {type: String, required: true},
+	productSubCategory: {type: String, required: true},
+	storageConditions: {type: String, required: true},
+	description: {type: String, required: false},
+	image: {type: String, required: false},
 }, {timestamps: true});
 
 
