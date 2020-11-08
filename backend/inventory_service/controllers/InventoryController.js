ef3d4c247bad7fb6f32081a510ae14724d7607b8
hsk007 <3259260-satz007@users.noreply.gitlab.com>
2020-11-08 18:45:57 +0000

    Update InventoryController.js

--- backend/inventory_service/controllers/InventoryController.js
+++ backend/inventory_service/controllers/InventoryController.js
@@ -414,21 +414,7 @@ exports.getAllInventoryDetails = [
               const products_array = productNamesResponse.data.data.map(
                 product => product.productName,
               );
-              const dict = {};
 
-              for (i = 0; i < items.length; i++) {
-                const productName = JSON.parse(items[i].data).productName;
-                const count = parseInt(JSON.parse(items[i].data).quantity);
-                const index = products_array.indexOf(productName);
-                var name = products_array[index];
-                if (name in dict) {
-                  var exis = dict[name];
-                  var new_val = count;
-                  dict[name] = exis + new_val;
-                } else {
-                  dict[name] = count;
-                }
-              }
               logger.log(
                 'info',
                 '<<<<< InventoryService < InventoryController < getAllInventoryDetails : queried and pushed data',
@@ -556,7 +542,7 @@ exports.getAllInventoryDetails = [
               var products = await InventoryModel.aggregate([{ $match : { owner : address } } ,
               {"$group" : {"_id": "$productName","productName":{"$first":"$productName"}, "quantity": { "$sum": "$quantity" }}}
               ]);
-              var productCounts = {};
+              var dict = {};
 
               for (var j = 0; j < products.length; j++) {
               var productName = products[j].productName;
@@ -564,18 +550,18 @@ exports.getAllInventoryDetails = [
 
               const index = products_array.indexOf(productName);
               var name = products_array[index];
-                if (name in productCounts) {
-                  var exis = productCounts[name];
+                if (name in dict) {
+                  var exis = dict[name];
                   var new_val = count;
-                  productCounts[name] = exis + new_val;
+                  dict[name] = exis + new_val;
                 } else {
-                  productCounts[name] = count;
+                  dict[name] = count;
                 }
             }
 
               res.json({
                 data: items,
-                productCounts,
+                dict,
                 counts: {
                   inventoryAdded: {
                     total: total_inv,
