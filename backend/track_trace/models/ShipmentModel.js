a621ffc2ece8efee700057002a0ff7474e48ac66
hsk007 <3259260-satz007@users.noreply.gitlab.com>
2020-11-24 12:11:41 +0000

    Update ShipmentModel.js

--- backend/track_trace/models/ShipmentModel.js
+++ backend/track_trace/models/ShipmentModel.js
@@ -1,12 +1,11 @@
 var mongoose = require("mongoose");
 ShipmentSchema = new mongoose.Schema({
   shipmentId: {type: String, required: true, unique: true},
-  POId: {type: String},
-  txnIds: {type: Array, required: true},
-  receiver: {type: String, required: true},
-  sender: {type: String, required: true},
-  status: {type: String, required: true},
-  estimatedDeliveryDate: {type: String, required: true},
+	txnIds: {type: Array, required: true},
+	receiver: {type: String, required: true},
+	sender: {type: String, required: true},
+	status: {type: String, required: true},
+	estimatedDeliveryDate: {type: String, required: true},
 }, {timestamps: true});
 
 
