a0e7c38359fb9f4bb6aa758ab73a462525ae7912
Hemalatha <hema@statwig.com>
2021-01-25 18:57:07 +0000

    Update openApiDocumentation.js

--- backend/employee_service/openApiDocumentation.js
+++ backend/employee_service/openApiDocumentation.js
@@ -19,15 +19,15 @@ module.exports = {
   },
   servers: [
     {
-      url: 'http://localhost:3001/',
+      url: 'http://localhost:3001',
       description: 'Local server'
     },
     {
-      url: 'https://test.vaccineledger.com:3001/',
+      url: 'http://test.vaccineledger.com:9001',
       description: 'Testing server'
     },
     {
-      url: 'https://vaccineledger.com:3001/',
+      url: 'http://api.vaccineledger.com:9001',
       description: 'Production server'
     }
   ],
