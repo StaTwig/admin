18029bedf1e727a5e350b22cee2bfb6678baee0c
Ravilochan <ravilochan2000@gmail.com>
2021-01-28 23:37:08 +0530

    create Shipment

--- backend/shipment_order_service/routes/shipment.js
+++ backend/shipment_order_service/routes/shipment.js
@@ -16,12 +16,12 @@ const upload = multer({ storage: Storage });
 const router = express.Router();
 
 router.post("/createShipment", ShipmentController.createShipment);
-router.get("/fetchShipments/:id", ShipmentController.fetchShipments);
-router.get("/shipment/:id", ShipmentController.Shipment); // Individual Shipment by _id
+router.get("/fetchShipments", ShipmentController.fetchShipments); // /fetchShipments?id=123
+// router.get("/shipment", ShipmentController.Shipment); // Individual Shipment by _id of MONGO /shipment?id=123
 
 router.get("/fetchShipments/:po_id", ShipmentController.fetch_po_Shipments);
 router.get("/fetchAllShipments", ShipmentController.fetchAllShipments);
-//router.post("/updateShipmentStatus/:id", ShipmentController.updateStatus);
+router.post("/updateShipmentStatus", ShipmentController.updateStatus); // /updateShipmentStatus?id=123
 
 module.exports = router;
 
