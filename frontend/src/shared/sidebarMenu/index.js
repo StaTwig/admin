859770dc283e63da35eb2101fe1850b3643fbc1f
harsha <harsha_d@statwig.com>
2021-02-10 17:59:38 +0530

    fixed hovering issue in sidebar

--- frontend/src/shared/sidebarMenu/index.js
+++ frontend/src/shared/sidebarMenu/index.js
@@ -39,9 +39,9 @@ const SideBar = ({
             <span>Dashboard</span>
           </Link>
         </li>
-        <li className={(url === '/shipments' || url === '/newshipment'||'/transactionHistory') ? 'active' : ''}>
+        <li className={(url === '/shipments' || url === '/newshipment'|| url === '/transactionHistory') ? 'active' : ''}>
           <Link to="/shipments">
-            <img src={(url === '/shipments' || url === '/newshipment'||'/transactionHistory')  ? shipSelectedIcon : shipIcon} alt="Shippment" />
+            <img src={(url === '/shipments' || url === '/newshipment'|| url === '/transactionHistory')  ? shipSelectedIcon : shipIcon} alt="Shippment" />
             <span>Shipment</span>
           </Link>
         </li>
