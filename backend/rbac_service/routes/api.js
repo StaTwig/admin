28db7525f0c064af6535feac2ec6076b0c94172c
Ravilochan <ravilochan2000@gmail.com>
2021-02-22 07:10:01 +0530

    rbac index

--- backend/rbac_service/routes/api.js
+++ backend/rbac_service/routes/api.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const RbacController = require("../controllers/RbacController");
 const app = express();
+app.get("/",(req,res)=> res.send({status:"OK"}))
 app.get("/getPermissions", RbacController.getPermissions);
 app.post("/addPermissions", RbacController.addPermissions);
 module.exports = app;
