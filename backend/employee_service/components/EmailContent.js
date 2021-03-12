790fafa6751fd5bc13c121837c5e208da1ab6eb9
Ashwini Shankar Patankar <ashwinispatankar@gmail.com>
2021-03-12 10:38:58 +0530

    [DevOps] updated the email template for statledger and made it more generic

--- backend/employee_service/components/EmailContent.js
+++ backend/employee_service/components/EmailContent.js
@@ -1,7 +1,7 @@
 const EmailContent = ({ name, origin, otp }) => {
   const verifyLink = `${origin}/verify`;
   return `<head>
-        <title>VaccineLedger</title>
+        <title>StatLedger</title>
         <style>
             body {
                 font-family: Helvetica, sans-serif;
@@ -15,16 +15,16 @@ const EmailContent = ({ name, origin, otp }) => {
             </div>
             <div class="email-body">
                 <h4>Dear ${name},</h4>
-                <h2>Welcome to VaccineLedger!</h2>
-                <p>To help you get started using VaccineLedger, we need to make sure that we have your right email address.</p>
+                <h2>Welcome to StatLedger!</h2>
+                <p>To help you get started using StatLedger, we need to make sure that we have your right email address.</p>
                 <p>By verifying your email just once, you can use all of our services!</p>
                 <span style="margin-bottom:15px"></span>
                 <h2>OTP - ${otp}</h2>
                 <span style="margin-bottom:15px"></span>
                 <a href=${verifyLink}>${verifyLink}</a>
                 <span style="margin-bottom:15px"></span>
-                <h2>What is VaccineLedger?</h2>
-                <p>VaccineLedger is a Blockchain based platform, to track and trace vaccine's journey across the suplly chain.</p>
+                <h2>What is StatLedger?</h2>
+                <p>StatLedger is a Blockchain based platform, to track and trace the products's journey across the supply chain.</p>
                 <span style="margin-bottom:15px"></span>
                 <p>
                     Please mail us at <a href="mailto:contactus@statwig.com">contactus@statwig.com</a> in case of any queries or if you've not initiated this request.
