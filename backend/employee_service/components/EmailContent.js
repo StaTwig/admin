f8ae779a76a8ce7b256cf9cef7b99860dd5a11bd
Ashwini Shankar Patankar <ashwinispatankar@gmail.com>
2021-03-15 12:45:13 +0530

    changed the email content to minimal

--- backend/employee_service/components/EmailContent.js
+++ backend/employee_service/components/EmailContent.js
@@ -1,7 +1,7 @@
 const EmailContent = ({ name, origin, otp }) => {
   const verifyLink = `${origin}/verify`;
   return `<head>
-        <title>StatLedger</title>
+        <title>OTP</title>
         <style>
             body {
                 font-family: Helvetica, sans-serif;
@@ -10,21 +10,18 @@ const EmailContent = ({ name, origin, otp }) => {
     </head>
     <body>
         <div class="wrapper">
-            <div class="logo">
-                <img src="http://test.vaccineledger.com/dist/97b8f67db5b802bef872b0d34d7baf06.png" style="width: 200px;"/>
-            </div>
+            
             <div class="email-body">
                 <h4>Dear ${name},</h4>
-                <h2>Welcome to StatLedger!</h2>
-                <p>To help you get started using StatLedger, we need to make sure that we have your right email address.</p>
-                <p>By verifying your email just once, you can use all of our services!</p>
+                
+                
+                <p>To verify please use the below OTP</p>
                 <span style="margin-bottom:15px"></span>
                 <h2>OTP - ${otp}</h2>
                 <span style="margin-bottom:15px"></span>
                 <a href=${verifyLink}>${verifyLink}</a>
                 <span style="margin-bottom:15px"></span>
-                <h2>What is StatLedger?</h2>
-                <p>StatLedger is a Blockchain based platform, to track and trace the products's journey across the supply chain.</p>
+                
                 <span style="margin-bottom:15px"></span>
                 <p>
                     Please mail us at <a href="mailto:contactus@statwig.com">contactus@statwig.com</a> in case of any queries or if you've not initiated this request.
