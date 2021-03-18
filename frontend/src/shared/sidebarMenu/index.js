f0a33ca18a6b55345a9d57d4ce220c9e1a3cf053
Hemalatha <hema@statwig.com>
2021-03-18 18:11:52 +0000

    Update index.js

--- frontend/src/shared/sidebarMenu/index.js
+++ frontend/src/shared/sidebarMenu/index.js
@@ -18,7 +18,9 @@ import trackSelectedIcon from '../../assets/icons/Track_Traceselected.png';
 import NetworkSelectedIcon from '../../assets/icons/NetworkSelected.svg';
 import UtilitiesSelected from '../../assets/icons/utilitieswhite.png';
 import UtilitiesIcon from '../../assets/icons/utilitiesblue.png';
-import OrderSelectedIcon from '../../assets/icons/OrderSelected.png';
+import OrderSelectedIcon from '../../assets/icons/orderSelected.png';
+import OrderIcon from '../../assets/icons/Orders.png';
+
 import './style.scss'
 const SideBar = ({
   match,
@@ -37,7 +39,7 @@ const SideBar = ({
 
 <li className={(url === '/shipments' || url === '/newshipment'|| url === '/transactionHistory') ? 'active' : ''}>
           <Link to="/shipments">
-            <img src={(url === '/shipments' || url === '/newshipment'|| url === '/transactionHistory')  ? OrderSelectedIcon : OrderSelectedIcon} alt="Shippment" />
+            <img src={(url === '/shipments' || url === '/newshipment'|| url === '/transactionHistory')  ? OrderSelectedIcon : OrderIcon} alt="Shippment" />
             <span>Orders</span>
           </Link>
         </li>
