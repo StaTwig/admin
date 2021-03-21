176e9529a7e9120ab1f0f0d2e813aa9560470bec
Anilkumar K <anil.ani88@gmail.com>
2021-03-21 17:06:57 +0530

    created sign up and OTP screens

---
+++ AbInBev/frontend/src/components/home/verifyPassword.js
@@ -0,0 +1,37 @@
+import React from "react";
+import logo from "../../assets/ABInBev.png";
+
+const VerifyPassword = (props) => {
+  const { setSteps, steps } = props;
+  return (
+    <div className="VerifyPasswordScreen">
+      <div className="align-center pb-5 pt-5">
+        <h2 className="titleSubHeading">Welcome Back!</h2>
+        <span className="titleSubHeading"><span className="titleHeading">Login</span> to continue.</span>
+      </div>
+
+      <div className="loginUserBlock justify-content-center">
+        <div className="form-group">
+          <label htmlFor="username" className="userNameLabel">Username/ Mobile No.</label>
+          <input name="username" className="form-control username" />
+          <button
+            onClick={() => {
+              setContinueClick(true);
+              setSteps(3);
+            }}
+            className={`width100 btn mt-4`}
+            type="button"
+          >
+            Send OTP
+          </button>
+          <p className="signUpDesc align-center mt-3 ">Don't have an account? <b>Sign Up</b></p>
+        </div>
+      </div>
+      <div className="col text-center footer-logo">
+        <img src={logo} width={60} />
+      </div>
+    </div>
+  );
+};
+
+export default VerifyPassword;
