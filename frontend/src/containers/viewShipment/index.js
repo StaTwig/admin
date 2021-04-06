45b7b489a9296b863a0aaa189578821d262d1e0d
Hemalatha <hema@statwig.com>
2021-04-06 22:27:59 +0000

    Update index.js

--- frontend/src/containers/viewShipment/index.js
+++ frontend/src/containers/viewShipment/index.js
@@ -2,13 +2,11 @@ import React, { useEffect, useState } from 'react';
 import ViewShipment from '../../components/viewShipment';
 import Header from '../../shared/header';
 import Sidebar from '../../shared/sidebarMenu';
-import { useDispatch, useSelector } from 'react-redux';
-import { getViewShipment } from '../../actions/shipmentActions';
 import {trackProduct} from "../../actions/shipmentActions";
-
+import { chainOfCustody, updateStatus } from "../../actions/shipmentActions";
 const ViewShipmentContainer = props => {
   const[trackData,setTrackData]=useState({});
-
+const [shippmentChainOfCustodyData, setShippmentChainOfCustodyData] = useState([]);
    useEffect(() => {
     async function fetchData() {
       const result = await trackProduct(props.match.params.id);
@@ -21,14 +19,25 @@ const ViewShipmentContainer = props => {
 }
     fetchData();
   },[]);
-
+ useEffect(() => {
+    async function fetchData() {
+      const result = await chainOfCustody(props.match.params.id);
+       if (result.status==200)
+       {
+      setShippmentChainOfCustodyData(result.data.data['shipmentChainOfCustody']);
+       }else{
+         setShippmentChainOfCustodyData([]);
+       }
+}
+    fetchData();
+  },[]);
   return (
     <div className="container-fluid p-0">
       <Header {...props} />
       <div className="d-flex">
         <Sidebar {...props} />
         <div className="content">
-          < ViewShipment trackData={trackData} {...props}/>
+          < ViewShipment trackData={trackData} shippmentChainOfCustodyData={shippmentChainOfCustodyData}{...props}/>
         </div>
       </div>
     </div>
