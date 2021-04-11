ecfe41c3c321cace34478731489a3cb2f2938fff
Hemalatha <hema@statwig.com>
2021-04-11 21:09:12 +0000

    Update POController.js

--- backend/po_service/controllers/POController.js
+++ backend/po_service/controllers/POController.js
@@ -362,21 +362,21 @@ exports.changePOStatus = [
             if (permissionResult.success) {
               try {
                 const { address } = req.user;
-                const { orderID } = req.body;
+                const { orderID, status } = req.body;
                 const po = await RecordModel.findOne({ id : orderID });
                 if (po && po.customer.customer_incharge === address) {
                   
                 const currDateTime = date.format( new Date(), 'DD/MM/YYYY HH:mm');
                 const updates = {
                      "updatedOn": currDateTime,
-                     "status":req.body.poStatus,
+                     "status":status
                 }
 
                 const updateData = await RecordModel.findOneAndUpdate(
                 { id: orderID },
                 {
                       $push: { poUpdates: updates },
-                      $set: {poStatus :req.body.poStatus }
+                      $set: {poStatus :status }
                 })
 
                 return apiResponse.successResponseWithData(
