9be889ba062af2e4b4b71b2c634c4cde2e15fd84
Hemalatha <hema@statwig.com>
2021-05-12 13:16:13 +0000

    Update revieworderpopup.js

--- frontend/src/components/revieworder/revieworderpopup.js
+++ frontend/src/components/revieworder/revieworderpopup.js
@@ -8,14 +8,15 @@ const ReviewOrderPopUp = (props) => {
       <div className="d-flex  flex-column align-items-center">
         <img src={Checked} width='60' height='60' className="mb-3" />
         <div className="alert" >
-          Success!
+         <b> Success!</b>
         </div>
         <div className="data">
-         Your Order<span className="bold">{props.OrderId}</span>
+        The New Order has been Created
         </div>
         <div className="data mb-4">
-          Sent Successfully!
+         Successfully!
         </div>
+         <div className="data mb-4">Order ID-<b>{props.OrderId}</b></div>
         <button className="btn-primary btn" onClick={props.onHide}>OK</button>
       </div>
     </div>
