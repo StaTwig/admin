7b860d24bf316085c6099d1869b70000cf7e46b4
Tushar Bajaj <tbajaj@iitg.ac.in>
2021-05-25 17:37:55 +0530

    fixed add location

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -379,6 +379,7 @@ export function config() {
       GetEOLInfoByPlaceAdministered:`${TEST_SERVER_URL}/lastmilemanagement/api/GetEOLInfoByPlaceAdministered?place=`,
       GetEOLListByDateWindow:`${TEST_SERVER_URL}/lastmilemanagement/api/GetEOLListByDateWindow`,
       getWarehouseById: `${TEST_SERVER_URL}/usermanagement/api/auth/getwarehouseinfo?id=`,
+      addWarehouse: `${DEMO_SERVER_URL}/usermanagement/api/auth/addWarehouse`,
     },
     demo: {
       loginUrl: `${DEMO_SERVER_URL}/usermanagement/api/auth/login`,
