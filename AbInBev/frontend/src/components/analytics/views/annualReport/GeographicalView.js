e0626930c38ad93938133db010d5d794d41357f3
Maheshkumar G <Maheshkumarg@apples-MacBook-Pro.local>
2021-06-13 12:42:54 +0530

    Merge conflicts

--- AbInBev/frontend/src/components/analytics/views/annualReport/GeographicalView.js
+++ AbInBev/frontend/src/components/analytics/views/annualReport/GeographicalView.js
@@ -81,7 +81,7 @@ const GeographicalView = (props) => {
             <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mb-3">
                 <h1 className="h2">Analytics</h1>
             </div>
-            <div className="btn-group mainButtonFilter">
+            {/*<div className="btn-group mainButtonFilter">
                 <a href="#!" className="btn active">State</a>
                 <select className="btn selectState" onChange="getSKUByState($event)">
                     <option>Select state</option>
@@ -92,7 +92,7 @@ const GeographicalView = (props) => {
                 </select>
             </div>
 
-            {/* <div className="row">
+             <div className="row">
                 <div className="col-md-3 ">
                     <div className="analyticsCard">
                         <span className="analyticsTitle">Sales</span>
