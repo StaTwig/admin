4d4755eea73a4eb794906db76c5510b51ed932f1
rajpratik8484 <44210415+rajpratik8484@users.noreply.github.com>
2021-06-14 23:50:46 +0530

    viewOrder_UI

--- frontend/src/components/vieworder/index.js
+++ frontend/src/components/vieworder/index.js
@@ -55,32 +55,32 @@ const onPOStatusChange = async status => {
 <Link to={`/orders`}>
  <button className="btn btn-orange fontSize20 font-bold mr-4"  onClick={() => onPOStatusChange('REJECTED')} >Reject Order</button></Link>
 <Link to={`/orders`}>
-            <button className="btn btn-outline-primary mr-2" ><img src={back} height="17" className="mr-2 mb-1" />Back to Orders</button>
+            <button className="btn btn-outline-primary mt-2" ><img src={back} height="17" className="mr-2 mb-1" />Back to Orders</button>
           </Link>
         </div>):(<div className="d-flex">
           <Link to={`/orders`}>
-            <button className="btn btn-outline-primary mr-2" ><img src={back} height="17" className="mr-2 mb-1" />Back to Orders</button>
+            <button className="btn btn-outline-primary mt-2" ><img src={back} height="17" className="mr-2 mb-1" />Back to Orders</button>
           </Link>
         </div>)}
       </div>
-      <div className="mt-4">
-        <div className="row bg-white shadow  p-3 m-3">
-          <div className="col row pl-4">
-            <span className="col-4">Order ID:</span>
+      <div className="mt-5">
+        <div className="row bg-white shadow  p-3" style={{position:"relative", marginTop:"-40px"}}>
+          <div className="col row">
+            <span className="col-4 ml-2">Order ID:</span>
             <div className="col">
               <span className="text-dark font-weight-bold">{order.id}</span>
-              <span className={`ml-2 p-1 status pl-2 plr-2 rounded text-white secondary-bg ${statusStyle}`}>
+              <span className={`ml-3 p-2 status rounded text-white secondary-bg ${statusStyle}`}>
                 {status}
                 </span>
               </div>
           </div>
-          <div className="col row pl-4">
+          <div className="col row">
             <span className="col-4">Order Date:</span>
             <span className="col text-dark font-weight-bold">{formatDate(order.creationDate)}</span>
           </div>
         </div>
-        <div className="row bg-white shadow p-3 m-3">
-          <div className=" pl-4 col-12">
+        <div className="row bg-white shadow mt-4 p-3">
+          <div className="col-12">
             <span className="p-1 text-primary font-weight-bold">Order From</span>
             <div>
               <div className=" row p-1">
@@ -96,11 +96,11 @@ const onPOStatusChange = async status => {
             </div>
           </div>
         </div>
-        <div className="row bg-white shadow p-3 m-3">
-          <div className=" pl-4 col-12">
+        <div className="row bg-white shadow p-3 mt-4">
+          <div className="col-12">
             <span className="p-1 text-primary font-weight-bold">Order To</span>
             <div>
-              <div className=" row p-1">
+              <div className="row p-1">
                 <div className="col row">
                   <span className="col-4">Organisation Name: </span>
                   <span className="col text-dark font-weight-bold">{order.customer?.organisation?.name}</span>
@@ -110,7 +110,7 @@ const onPOStatusChange = async status => {
                   <span className="col text-dark font-weight-bold">{order.customer?.organisation?.id}</span>
                 </div>
                 <div class="w-100"></div>
-                <div className="col row col-6 mt-2">      
+                <div className="col row col-6 mt-3">      
                   <span className="col-4">Delivery Location:</span>
                   <span className="col ml-2 text-dark font-weight-bold">{order && order.customer&& order.customer.warehouse && order.customer.warehouse.warehouseAddress ? order.customer.warehouse.title+ " / "+ order.customer.warehouse.warehouseAddress.firstLine + " "+order.customer.warehouse.warehouseAddress.city: null}</span>
                 </div>
@@ -118,11 +118,11 @@ const onPOStatusChange = async status => {
             </div>
           </div>
         </div>
-        <div className=" p-3 m-3">
+        <div className="m-3 ml-5 mt-5">
           <span className="p-1 text-info font-weight-bold">Product Details</span>
           <div className="row mt-3">
             {order?.products?.map((product, index) =>
-            <div className={`bg-white shadow ${index > 0 ? 'ml-4' : ''}  p-3`}>
+            <div className={`bg-white shadow ${index > 0 ? 'ml-5' : ''}  p-3`}>
               <span className="p-1 font-weight-normal text-primary font-weight-bold">{product.name}</span>
               <div className="row  p-1">
                 <span className="col">Product ID:</span>
