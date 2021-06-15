ac3711d5379fc99d2e811bc5e1e3b4f8fbf388d1
Hemalatha <hema@statwig.com>
2021-06-15 17:01:14 +0000

    Update index.js

--- frontend/src/shared/table/index.js
+++ frontend/src/shared/table/index.js
@@ -58,7 +58,7 @@ inventoryDetails.sort(function(a,b){
                   <div className="rTableCell " style={{position:"relative",left:'12%'}}> {formatDate(inventory.createdAt)}</div>
                   <div className="rTableCell"  style={{position:"relative",left:'19%'}}>{inventory.inventoryQuantity}</div>                                 
                   <div className="rTableCell" style={{position:"relative", left:"19%", fontWeight:" 600 "}}>
-                         {(inventory.eventTypePrimary !== 'ADD') ? (inventory.eventTypePrimary === 'RECEIVED' ? <div className="status secondary-bg bg-success"> RECEIVED</div> :  <div className="status secondary-bg bg-warning">SENT</div>) :  <div className="status secondary-bg bg-primary">ADDED</div>} 
+                         {(inventory.eventTypePrimary !== 'ADD') ? (inventory.eventTypePrimary === 'RECEIVE' ? <div className="status secondary-bg bg-success"> RECEIVED</div> :  <div className="status secondary-bg bg-warning">SENT</div>) :  <div className="status secondary-bg bg-primary">ADDED</div>} 
                   </div>
                     <div className=" rTableCell m-2" 
                          style={{position:"relative",left:'12%'}}>
@@ -122,7 +122,7 @@ inventoryDetails.sort(function(a,b){
                                       </div> }    
 
                                       <div className="mt-3" style={{position:"absolute ", left:"79% ", bottom:"38% ", width:"20% "}}>
-                                      {(inventory.eventTypePrimary === 'CREATE')  ? 
+                                      {(inventory.eventTypePrimary === 'CREATE'|| inventory.eventTypePrimary === 'RECEIVE')  ? 
                                       <button
                                           type="button" className="btn btn-outline-primary"  
                                           onClick={() => {
