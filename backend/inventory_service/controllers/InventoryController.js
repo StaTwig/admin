ff6962fbb0da2679004a98188b4f16972369b465
Sourav Dey <sourav.lko@gmail.com>
2021-06-28 16:33:41 +0530

    UOM added in getProductDetailsByWarehouseId

--- backend/inventory_service/controllers/InventoryController.js
+++ backend/inventory_service/controllers/InventoryController.js
@@ -2013,6 +2013,7 @@ exports.getProductDetailsByWarehouseId = [
           productId: product[0].id,
           manufacturer: product[0].manufacturer,
           quantity: list[j].quantity ? list[j].quantity : 0,
+          unitofMeasure:product[0].unitofMeasure
         };
         productArray.push(product1);
       }
