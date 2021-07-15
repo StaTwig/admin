51e4af1fea45b5fb72f09509028747462579fd8c
deepakyadav56617 <50566143+deepakyadav56617@users.noreply.github.com>
2021-07-15 08:30:17 +0530

    Added region,country,state,city dropdown

--- frontend/src/components/newaddress/index.js
+++ frontend/src/components/newaddress/index.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import "leaflet/dist/leaflet.css";
 import AddressField from "./addressfield";
+import TextField from '@material-ui/core/TextField';
+import Autocomplete from '@material-ui/lab/Autocomplete';
 import { Formik } from "formik";
 import FailedPopUp from "../../shared/PopUp/failedPopUp";
 import SuccessPopUp from "../../shared/PopUp/successPopUp";
@@ -8,6 +10,7 @@ import {
   getAddressByLatLong,
   addAddress,
   getWareHouses,
+  fetchAllRegions,fetchCountriesByRegion,fetchStateByCountry,fetchCitiesByState,
 } from "../../actions/organisationActions";
 import Modal from "../../shared/modal";
 import { turnOn, turnOff } from "../../actions/spinnerActions";
@@ -15,19 +18,50 @@ import { useSelector, useDispatch } from "react-redux";
 
 const NewAddress = (props) => {
   // const editAddress = JSON.parse(props.match.params.address);
+  useEffect(()=>{
+    async function fetchAllRegions1(){
+      let arr = await fetchAllRegions();
+      setallregions(arr.data);
+    }
+    fetchAllRegions1();
+  },[]);
 
   let editAddress;
   const dispatch = useDispatch();
   const [showModal, setShowModal] = useState(false);
   const [showModals, setShowModals] = useState(false);
   const [addr, setAdd] = useState({});
-  const [message, setMessage] = useState(
-    "Location service is disabled. Enter address manually!!!"
-  );
+  const [message, setMessage] = useState("Location service is disabled. Enter address manually!!!");
   const [address, setAddress] = useState({});
   const [pos, setPos] = useState({});
   const closeModal = () => setShowModal(false);
   const closeModals = () => setShowModals(false);
+
+  const [addressTitle, setAddressTitle] = useState("");
+  const [pincode, setPincode] = useState("");
+  const [region,setregion] = useState("");
+  const [country, setcountry] = useState("");
+  const [city, setCity] = useState("");
+  const [state, setState] = useState("");
+  const [addressLine, setAddressLine] = useState("");
+
+  const [allregions,setallregions] = useState([]);
+  const [allCountries,setallCountries] = useState([]);
+  const [allState,setallState] = useState([]);
+  const [allCity,setallCity] = useState([]);
+
+  async function fetchAllCountries1(id){
+    let res = await fetchCountriesByRegion(id);
+    setallCountries(res.data);
+  };
+  async function fetchAllState1(id){
+    let res = await fetchStateByCountry(id);
+    setallState(res.data);
+  };
+  async function fetchAllCity1(id){
+    let res = await fetchCitiesByState(id);
+    setallCity(res.data);
+  };
 
   const addArr = useSelector((state) => {
     return state.organisation.addresses;
@@ -81,7 +115,6 @@ const NewAddress = (props) => {
     );
     if (editAddress?.length) setAdd((a) => editAddress[0]);
   }
-
   useEffect(() => {
     if (
       addArr.length &&
@@ -94,7 +127,13 @@ const NewAddress = (props) => {
       if (editAddress?.length) setAdd(editAddress[0]);
     }
   });
-
+  function search(name, myArray){
+    for (var i=0; i < myArray.length; i++) {
+        if (myArray[i].name === name) {
+            return myArray[i].id;
+        }
+    }
+}
   return (
     <div className="address">
       {showModal && (
@@ -115,7 +154,10 @@ const NewAddress = (props) => {
           <SuccessPopUp onHide={closeModals} message={message} />
         </Modal>
       )}
-      <h1 className="breadcrumb dash pl-2">ADD NEW ADDRESS</h1>
+      {
+        Object.keys(addr).length == 0 ? <h1 className="breadcrumb dash pl-2">ADD NEW ADDRESS</h1>:
+        <h1 className="breadcrumb dash pl-2">EDIT ADDRESS</h1>
+      }
       <div className="d-flex row ">
         <div className="card w-100 rounded border border-white shadow bg-white m-4 p-3">
           <div className="card-body d-flex flex-row justify-content-between">
@@ -123,38 +165,39 @@ const NewAddress = (props) => {
               <Formik
                 enableReinitialize={true}
                 initialValues={{
-                  title: Object.keys(addr).length ? addr?.title : "Warehouse",
+                  title: Object.keys(addr).length ? addr?.title : addressTitle,
+                  region:Object.keys(addr).length? addr?.region?.regionName: region,
                   // flatno: addr?.length ? addr[0] : "",
                   pincode: address?.PostalCode
                     ? address?.PostalCode
                     : Object.keys(addr).length
                     ? addr?.warehouseAddress?.zipCode
-                    : "",
+                    : pincode,
                   area: address?.Subdistrict
                     ? address?.Subdistrict
                     : Object.keys(addr).length
                     ? addr?.warehouseAddress?.firstLine
-                    : "",
+                    : addressLine,
                   // landmark: addr?.length ? addr[2] : "",
                   town: address?.City
                     ? address?.City
                     : Object.keys(addr).length
                     ? addr?.warehouseAddress?.city
-                    : "",
+                    : city,
                   state: address?.AdditionalData?.length
                     ? address?.AdditionalData?.filter(
                         (row) => row.key == "StateName"
                       )[0].value
                     : Object.keys(addr).length
                     ? addr?.warehouseAddress?.state
-                    : "",
+                    : state,
                   country: address?.AdditionalData?.length
                     ? address?.AdditionalData?.filter(
                         (row) => row.key == "CountryName"
                       )[0].value
                     : Object.keys(addr).length
                     ? addr?.warehouseAddress?.country
-                    : "",
+                    : country,
                 }}
                 validate={(values) => {
                   const errors = {};
@@ -164,15 +207,12 @@ const NewAddress = (props) => {
                   if (!values.pincode) {
                     errors.pincode = "Required";
                   }
-                  // if (!values.flatno) {
-                  //   errors.flatno = "Required";
-                  // }
+                  if (!values.region) {
+                    errors.region = "Required";
+                  }
                   if (!values.area) {
                     errors.area = "Required";
                   }
-                  // if (!values.landmark) {
-                  //   errors.landmark = "Required";
-                  // }
                   if (!values.town) {
                     errors.town = "Required";
                   }
@@ -200,8 +240,149 @@ const NewAddress = (props) => {
                   setFieldValue,
                   dirty,
                 }) => (
-                  <form onSubmit={handleSubmit} className="mb-3">
-                    <AddressField
+                  <form onSubmit={handleSubmit} className="mb-3" enableReinitialize>
+                        <Autocomplete
+                          value={values.region}
+                          onBlur={handleBlur}
+                          onChange={(event, newValue) => {
+                            setFieldValue("region",newValue);
+                            fetchAllCountries1(newValue);
+                            setregion(newValue);
+                            setcountry("");
+                            setState("");
+                            setCity("");
+                          }}
+                          id="controllable-states-demo"               
+                          options={allregions}
+                          style={{ width: 300 }}
+                          renderInput={(params) => <TextField style={{
+                            width:"425px"
+                        }}{...params} label="Select Region"  />}
+                        />
+                        {errors.region && touched.region && (
+                          <span className="error-msg text-danger-ANL">
+                            {errors.region}
+                          </span>
+                        )}
+                        <Autocomplete
+                          value={values.country}
+                          onChange={(event, newValue) => {
+                            setFieldValue("country",newValue);
+                            let v = search(newValue,allCountries);
+                            fetchAllState1(v);
+                            setcountry(newValue);
+                            setState("");
+                            setCity("");
+                          }}
+                          id="controllable-states-demo"
+                          options={allCountries.map((option)=>option.name)}
+                          style={{ width: 300 }}
+                          renderInput={(params) => <TextField style={{
+                            width:"425px"
+                        }} {...params} label="Select Country"  />}
+                        />
+                        {errors.country && touched.country && (
+                          <span className="error-msg text-danger-ANL">
+                            {errors.country}
+                          </span>
+                        )}
+                        <Autocomplete
+                          onBlur={handleBlur}
+                          value={values.state}
+                          onChange={(event, newValue) => {
+                            setFieldValue("state",newValue);
+                            let v = search(newValue,allState);
+                            fetchAllCity1(v);
+                            setState(newValue);
+                            setCity("");
+                          }}
+                          id="controllable-states-demo"
+                          options={allState.map((option)=>option.name)}
+                          style={{ width: 300 }}
+                          renderInput={(params) => < TextField style={{
+                            width:"425px"
+                        }}{...params} label="Select State"  />}
+                        />
+                        {errors.state && touched.state && (
+                          <span className="error-msg text-danger-ANL">
+                            {errors.state}
+                          </span>
+                        )}
+                        <Autocomplete
+                          value={values.town}
+                          onChange={(event, newValue) => {
+                            setFieldValue("town",newValue);
+                            setCity(newValue);
+                          }}
+                          id="controllable-states-demo"
+                          options={allCity.map((Option)=>Option.name)}
+                          style={{ width: 300 }}
+                          renderInput={(params) => <TextField style={{
+                            width:"425px"
+                        }} {...params} label="Select City"  />}
+                        />
+                        {errors.town && touched.town && (
+                          <span className="error-msg text-danger-ANL">
+                            {errors.town}
+                          </span>
+                        )}
+                        <TextField 
+                          style={{
+                              width:"425px"
+                          }}
+                          id="standard-basic"
+                          label="Title" 
+                          className="form-control2"
+                          name="title"
+                          value={values.title}
+                          // handleChange={handleChange}
+                          onBlur={handleBlur}
+                          error={errors.title}
+                          // touched={touched.title}
+                          onChange={(e) => {handleChange(e);setAddressTitle(e.target.value)}}
+                        />
+                        {errors.title && touched.title && (
+                        <span className="error-msg text-dangerS">{errors.title}</span>
+                        )}  
+                  <TextField 
+                    style={{
+                        width:"425px"
+                    }}
+                    id="standard-basic"
+                    label="Address Line" 
+                    className="form-control2"
+                    name="area"
+                    value={values.area}
+                    // handleChange={handleChange}
+                    onBlur={handleBlur}
+                    error={errors.area}
+                    // touched={touched.area}
+                    onChange={(e) => {handleChange(e);setAddressLine(e.target.value)}}
+                    />
+                    {errors.area && touched.area && (
+                    <span className="error-msg text-dangerS">{errors.area}</span>
+                  )}  
+
+                <TextField 
+                  style={{
+                      width:"425px"
+                  }}
+                  id="standard-basic"
+                  label="Pin Code" 
+                  type="number"
+                  className="form-control2"
+                  name="pincode"
+                  value={values.pincode}
+                  handleChange={handleChange}
+                  handleBlur={handleBlur}
+                  error={errors.pincode}
+                  touched={touched.pincode}
+                  onChange={(e) => {handleChange(e); setPincode(e.target.value)}}
+                  />
+                  {errors.pincode && touched.pincode && (
+                  <span className="error-msg text-dangerS">{errors.pincode}</span>
+                  )}
+                    {/* <AddressField
                       error={errors.title}
                       touched={touched.title}
                       label="Address Title"
@@ -209,7 +390,7 @@ const NewAddress = (props) => {
                       handleChange={handleChange}
                       handleBlur={handleBlur}
                       value={values.title}
-                    />
+                    /> */}
                     {/* <AddressField
                       error={errors.flatno}
                       touched={touched.flatno}
@@ -219,7 +400,7 @@ const NewAddress = (props) => {
                       handleBlur={handleBlur}
                       value={values.flatno}
                     /> */}
-                    <AddressField
+                    {/* <AddressField
                       error={errors.area}
                       touched={touched.area}
                       label="Address line"
@@ -227,7 +408,7 @@ const NewAddress = (props) => {
                       handleChange={handleChange}
                       handleBlur={handleBlur}
                       value={values.area}
-                    />
+                    /> */}
                     {/* <AddressField
                       error={errors.landmark}
                       touched={touched.landmark}
@@ -237,7 +418,7 @@ const NewAddress = (props) => {
                       handleBlur={handleBlur}
                       value={values.landmark}
                     /> */}
-                    <AddressField
+                    {/* <AddressField
                       error={errors.town}
                       touched={touched.town}
                       label="City/ Town"
@@ -245,8 +426,8 @@ const NewAddress = (props) => {
                       handleChange={handleChange}
                       handleBlur={handleBlur}
                       value={values.town}
-                    />
-                    <AddressField
+                    /> */}
+                    {/* <AddressField
                       error={errors.state}
                       touched={touched.state}
                       label="State"
@@ -263,8 +444,8 @@ const NewAddress = (props) => {
                       handleChange={handleChange}
                       handleBlur={handleBlur}
                       value={values.country}
-                    />
-                    <AddressField
+                    /> */}
+                    {/* <AddressField
                       error={errors.pincode}
                       touched={touched.pincode}
                       label="Pincode"
@@ -272,7 +453,7 @@ const NewAddress = (props) => {
                       handleChange={handleChange}
                       handleBlur={handleBlur}
                       value={values.pincode}
-                    />
+                    /> */}
                     <div className="pt-5">
                       <button type="submit" className="btn btn-warning ">
                         <i
