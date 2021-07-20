6be34bc093e1a43ec845643b8d9c2c37bf188e2a
satz007 <satheesh@statwig.com>
2021-07-20 08:48:33 +0000

    delete inventory event fix

--- backend/inventory_service/controllers/InventoryController.js
+++ backend/inventory_service/controllers/InventoryController.js
@@ -1272,7 +1272,6 @@ exports.addProductsToInventory = [
           event_data.stackholders.actororg.id = orgId || "null";
           event_data.stackholders.actororg.name = orgName || "null";
           event_data.stackholders.actororg.address = address || "null";
-          event_data.actorWarehouseId = req.user.warehouseId || "null";
           event_data.stackholders.ca.id = CENTRAL_AUTHORITY_ID || "null";
           event_data.stackholders.ca.name = CENTRAL_AUTHORITY_NAME || "null";
           event_data.stackholders.ca.address = CENTRAL_AUTHORITY_ADDRESS || "null";
@@ -3317,6 +3316,7 @@ exports.deleteProductsFromInventory = [
             event_data.stackholders.actororg.id = orgId || "null";
             event_data.stackholders.actororg.name = orgName || "null";
             event_data.stackholders.actororg.address = address || "null";
+	    event_data.actorWarehouseId = req.user.warehouseId || "null";
             event_data.stackholders.ca.id = CENTRAL_AUTHORITY_ID || "null";
             event_data.stackholders.ca.name = CENTRAL_AUTHORITY_NAME || "null";
             event_data.stackholders.ca.address = CENTRAL_AUTHORITY_ADDRESS || "null";
@@ -3325,12 +3325,14 @@ exports.deleteProductsFromInventory = [
             event_data.stackholders.secondorg.address = receiverAddress || "null";
 
             event_data.payload.data = payload;
+		console.log("event adta",event_data)
             async function compute(event_data) {
                 result = await logEvent(event_data);
+		    console.log("result",result)
                 return result;
             }
             compute(event_data).then((response) => {
-		    console.log("res",response)
+		    console.log("res1",response)
             });
 
             return apiResponse.successResponse(
