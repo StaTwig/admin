fff485fb9bc62f9ed9a979be7c010c39a9cc8e25
satz19 <69015591+satz19@users.noreply.github.com>
2021-08-04 18:57:39 +0400

    Revert "Location approval"

--- frontend/src/utils/commonHelper.js
+++
@@ -1,3 +0,0 @@
-export const getAddress = (address) => {
-  return address.firstLine +" "+ (address.secondLine ? address.secondLine : "") +" "+ address.city +" "+ address.state +" "+ address.zipCode +" "+ address.country;
-}
