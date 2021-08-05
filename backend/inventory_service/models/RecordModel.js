0843c8e30e12ea53ea30065444ea50cc588e4770
Saravanan Jodavula <jvs.ravanan@gmail.com>
2021-08-05 09:41:16 +0530

    autocomplete API working

---
+++ backend/inventory_service/models/RecordModel.js
@@ -0,0 +1,48 @@
+var mongoose = require('mongoose');
+var RecordSchema = new mongoose.Schema(
+  {
+    id: { type: String, required: true, unique: true },
+    externalId: { type: 'String' },
+    creationDate: {
+      type: String,
+    },
+    createdBy: {
+      type: String,
+    },
+    supplier: {
+      type: Object,
+    },
+    customer: {
+      type: Object,
+    },
+    products: {
+      type: Array,
+      default: [],
+    },
+    poStatus: { type: String, default: 'CREATED' },
+    poUpdates : { type: Array,default: []},
+    lastUpdatedBy: { type: String },
+    lastUpdatedOn: {
+      type: String,
+      required: false,
+    },
+    shippingOrders: {
+      type: Array,
+      required: false,
+      default: [],
+    },
+    shipments: {
+      type: Array,
+      required: false,
+      default: [],
+    },
+    createdBy: {
+      type: String,
+    },
+    lastUpdatedBy : {
+      type: String,
+    },
+  },
+  { timestamps: true },
+);
+module.exports = mongoose.model('Record', RecordSchema);
