b126865cda093ec86d0f6a029c090579a1a12bb3
Ravilochan <ravilochan2000@gmail.com>
2021-08-20 06:43:02 +0530

    SO Rbac v2

--- backend/shippingOrder_service/routes/index.js
+++ backend/shippingOrder_service/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function(req, res) {
-	res.render("index", { title: "Express" });
+	res.status(200).json({status:"OK", message:"SO Service is good"})
 });
 
 module.exports = router;
