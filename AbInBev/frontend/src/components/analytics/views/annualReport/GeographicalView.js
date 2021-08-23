44e0c95f3c31603a3e0f97f3d065fd698f2795cb
Saravanan Jodavula <jvs.ravanan@gmail.com>
2021-08-23 14:03:05 +0530

    Abinbev sort fix

--- AbInBev/frontend/src/components/analytics/views/annualReport/GeographicalView.js
+++ AbInBev/frontend/src/components/analytics/views/annualReport/GeographicalView.js
@@ -97,10 +97,10 @@ const GeographicalView = (props) => {
       let sortableItems = [...items];
       if (sortConfig !== null) {
         sortableItems.sort((a, b) => {
-          if (a[sortConfig.key] < b[sortConfig.key]) {
+          if (parseInt(a[sortConfig.key]) < parseInt(b[sortConfig.key])) {
             return sortConfig.direction === 'ascending' ? -1 : 1;
           }
-          if (a[sortConfig.key] > b[sortConfig.key]) {
+          if (parseInt(a[sortConfig.key]) > parseInt(b[sortConfig.key])) {
             return sortConfig.direction === 'ascending' ? 1 : -1;
           }
           return 0;
