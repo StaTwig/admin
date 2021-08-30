0ec9c4d8cd266bc5278bb833518075a8e3a73ed1
Maheshkumar G <Maheshkumarg@apples-MacBook-Pro.local>
2021-08-31 01:35:17 +0530

    Fix

--- frontend/src/shared/advanceTableFilter/index.js
+++ frontend/src/shared/advanceTableFilter/index.js
@@ -1172,6 +1172,13 @@ const AdvanceTableFilter = (props) => {
                   width="10" height="10"
                   className="ml-2"
                 />
+                {
+                  props.showExportFilter && props.exportFilterData &&
+                  <FilterDropDown
+                    data={props.exportFilterData}
+                    onChangeOfFilterDropDown={props.onSelectionOfDropdownValue}
+                    type={'export'}
+                  />
                   {
                     props.showExportFilter && props.exportFilterData &&
                     <FilterDropDown
