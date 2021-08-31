5b0d7a92e25e676813237fb98d975a675792ad71
rajpratik8484 <44210415+rajpratik8484@users.noreply.github.com>
2021-09-01 00:05:34 +0530

    Ui changes of Inventory table, product category, etc.

--- frontend/src/components/viewInventory/index.js
+++ frontend/src/components/viewInventory/index.js
@@ -8,6 +8,7 @@ import { useDispatch } from 'react-redux';
 import { getBatchDetailsByWareHouse } from '../../actions/inventoryActions';
 import { turnOn, turnOff } from '../../actions/spinnerActions';
 import { formatDate } from '../../utils/dateHelper';
+import Batch from '../../assets/icons/batch.png';
 
 const ViewInventory = props => {
   const [data, setData] = useState([]);
@@ -33,25 +34,25 @@ const ViewInventory = props => {
   return (
     <div className="viewinventory">
       <div className="d-flex justify-content-between">
-        <h1 className="breadcrumb">Inventory</h1>
+        <h1 className="breadcrumb">PRODUCT LIST</h1>
       </div>
       <div className="row">
-        <div className="p-3 full-width-ribbon">
+        <div className="p-2 full-width-ribbon">
           <div className="row filter">
             <div className="col-3">
-              <img src={Product} width="24" height="24" alt="Product Name" />
+              <img src={Product} width="16" height="16" alt="Product Name" />
               <span className="ml-2 font-small">Product Name</span>
             </div>
             <div className="col-3">
-              <img src={Quantity} width="35" height="24" alt="Product Category" />
+              <img src={Quantity} width="25" height="16" alt="Product Category" />
               <span className="ml-2 font-small">Product Category</span>
             </div>
             <div className="col-3">
-              <img src={user} width="16" height="24" alt="Manufacturer" />
+              <img src={user} width="16" height="16" alt="Manufacturer" />
               <span className="ml-2 font-small">Manufacturer</span>
             </div>
             <div className="col-2">
-              <img src={Quantity} width="35" height="24" alt="Quantity" />
+              <img src={Quantity} width="25" height="16" alt="Quantity" />
               <span className="ml-2 font-small">Quantity</span>
             </div>
           </div>
@@ -59,43 +60,43 @@ const ViewInventory = props => {
         <div className="w-100 pt-5 mt-2">
           {inventories.map((inv, i) => 
             <>
-              <div key={i} className="col-12 p-3 mb-3 rounded row bg-white shadow">
+              <div key={i} className="col-12 p-2 mb-3 rounded row bg-white shadow">
                 <div className="col-3 txt txtBlue">{inv.products.name}</div>
                 <div className="col-3 txt1 ml-3">{inv.products.type}</div>
                 <div className="col-3 txt1">{inv.products.manufacturer}</div>
                 <div className="col-2 txt1">{inv.inventoryDetails.quantity ? inv.inventoryDetails.quantity : 0}{" ("}{inv.products.unitofMeasure? inv.products.unitofMeasure.name:"N/A"}{")"}</div>
-                <div className="col-0 txt "><button type="button" onClick={() => toggleShowMore(inv.id, inv.products.id, i, more[i])} className="btn btn-outline-primary">Show {more[i] ? `less` :`more`}</button></div>
+                <div className="col-0 txt "><button type="button" onClick={() => toggleShowMore(inv.id, inv.products.id, i, more[i])} className="btn btn-outline-primary">Show {more[i] ? `less` : `more`}</button></div>
               </div>
               {more[i] &&
                 <div className="row">
-                  <div className="p-3 mt-4 full-width-ribbon">
+                  <div className="p-2 mt-4 full-width-ribbon">
                     <div className="row filter">
                       <div className="col-2">
-                        <img src={Product} width="24" height="24" alt="Product Name" />
+                        <img src={Product} width="16" height="16" alt="Product Name" />
                         <span className="ml-2 font-small">Product Name</span>
                       </div>
                       <div className="col">
-                        <img src={Quantity} width="35" height="24" alt="Product Category" />
+                        <img src={Quantity} width="25" height="16" alt="Product Category" />
                         <span className="ml-2 font-small">Product Category</span>
                       </div>
                       <div className="col">
-                        <img src={user} width="16" height="24" alt="Manufacturer" />
+                        <img src={user} width="16" height="16" alt="Manufacturer" />
                         <span className="ml-2 font-small">Manufacturer</span>
                       </div>
                       <div className="col p-0">
-                        <img src={Quantity} width="35" height="24" alt="Quantity" />
+                        <img src={Quantity} width="25" height="16" alt="Quantity" />
                         <span className="ml-2 font-small">Quantity</span>
                       </div>
                       <div className="col">
-                        <img src={Quantity} width="35" height="24" alt="Batch Number" />
+                        <img src={Batch} width="16" height="16" alt="Batch Number" />
                         <span className="ml-2 font-small">Batch Number</span>
                       </div>
                       <div className="col pl-0">
-                        <img src={calender} width="35" height="24" alt="Mfg Date" />
+                        <img src={calender} width="25" height="16" alt="Mfg Date" />
                         <span className="ml-1 font-small">Mfg Date</span>
                       </div>
                       <div className="col p-0">
-                        <img src={calender} width="35" height="24" alt="Exp Date" />
+                        <img src={calender} width="25" height="16" alt="Exp Date" />
                         <span className="ml-1 font-small">Exp Date</span>
                       </div>
                     </div>
