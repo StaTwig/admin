acc9c3ae4eee202530f117814c2b1050e4dc8dd3
Ravilochan <ravilochan2000@gmail.com>
2021-09-07 16:38:08 +0530

    Upsert instead of two operations

--- backend/rbac_service/controllers/RbacController.js
+++ backend/rbac_service/controllers/RbacController.js
@@ -1,24 +1,30 @@
-const RbacModel = require('../models/RbacModel');
-const { body, validationResult } = require('express-validator');
-const auth = require('../middlewares/jwt');
-const apiResponse = require('../helpers/apiResponse');
-const { default: axios } = require('axios');
-const dotenv = require('dotenv').config();
-
+const RbacModel = require("../models/RbacModel");
+const { body, validationResult } = require("express-validator");
+const auth = require("../middlewares/jwt");
+const apiResponse = require("../helpers/apiResponse");
+const { default: axios } = require("axios");
+const dotenv = require("dotenv").config();
 
 exports.getPermissions = [
   auth,
   async (req, res) => {
     try {
-          const { role } = req.query;
-          if(role){
-          const permissions = await RbacModel.find({role});
-          return apiResponse.successResponseWithData(res, `Permissions of ${role}`, permissions);
-          }
-          else{
-          const permissions = await RbacModel.find({});
-          return apiResponse.successResponseWithData(res, "All Permissions", permissions);
-          }     
+      const { role } = req.query;
+      if (role) {
+        const permissions = await RbacModel.find({ role });
+        return apiResponse.successResponseWithData(
+          res,
+          `Permissions of ${role}`,
+          permissions
+        );
+      } else {
+        const permissions = await RbacModel.find({});
+        return apiResponse.successResponseWithData(
+          res,
+          "All Permissions",
+          permissions
+        );
+      }
     } catch (err) {
       return apiResponse.ErrorResponse(res, err.message);
     }
@@ -29,68 +35,64 @@ exports.getRoles = [
   auth,
   async (req, res) => {
     try {
-          var roles =[];
-          const results = await RbacModel.find({}, { _id: 0, role: 1 });
-          results.map(element => {
-              roles.push(element.role);
-            })
-          return apiResponse.successResponseWithData(res, "All Roles", roles);
+      var roles = [];
+      const results = await RbacModel.find({}, { _id: 0, role: 1 });
+      results.map((element) => {
+        roles.push(element.role);
+      });
+      return apiResponse.successResponseWithData(res, "All Roles", roles);
     } catch (err) {
       return apiResponse.ErrorResponse(res, err);
     }
-  }
-]
+  },
+];
 
 exports.updatePermissions = [
-  // auth,
-  body('permissions')
+  auth,
+  body("permissions")
     .isLength({ min: 1 })
-    .withMessage('Permission Structure must be specified.'),
-  body('role')
+    .withMessage("Permission Structure must be specified."),
+  body("role")
     .isLength({ min: 1 })
     .trim()
-    .withMessage('Role must be specified.'),
+    .withMessage("Role must be specified."),
   async (req, res) => {
-    try{
+    try {
       const errors = validationResult(req);
       if (!errors.isEmpty()) {
         return apiResponse.validationErrorWithData(
           res,
-          'Validation Error.',
-          errors.array(),
+          "Validation Error.",
+          errors.array()
         );
       }
       const { role, permissions } = req.body;
       var permsArray = [];
-      for(var i in permissions){
+      for (var i in permissions) {
         for (const [key, value] of Object.entries(permissions[i])) {
-          if(value == true){
+          if (value == true) {
             permsArray.push(key);
           }
-          if(value == false){
-            permsArray = permsArray.filter(elem => elem !== key);
+          if (value == false) {
+            permsArray = permsArray.filter((elem) => elem !== key);
           }
-        } 
-    }
-      let rbac_object = await RbacModel.findOneAndUpdate({ role },{$set: permissions , "permissions": permsArray}, {new: true});
-      if(!rbac_object){
-      const rbac = new RbacModel({
-        role,
-        permissions : permsArray,
-      });
-      await rbac.save();
-      rbac_object = await RbacModel.findOneAndUpdate({ role },{$set: permissions}, {new: true});
-    }
-    console.log(rbac_object);
-    const result = await axios.get(process.env.LEDGER + '/rbacmanagement/api/rbacCache');
-    if(result.data == undefined){
-      return apiResponse.errorResponse(res, result.data);
-    }
-    return apiResponse.successResponseWithData(res, 'Success', rbac_object);
-  }
-    catch (err) {
+        }
+      }
+      let rbac_object = await RbacModel.findOneAndUpdate(
+        { role },
+        { $set: permissions, permissions: permsArray },
+        { new: true, upsert: true }
+      );
+      const result = await axios.get(
+        process.env.LEDGER + "/rbacmanagement/api/rbacCache"
+      );
+      if (result.data == undefined) {
+        return apiResponse.errorResponse(res, result.data);
+      }
+      return apiResponse.successResponseWithData(res, "Success", rbac_object);
+    } catch (err) {
       console.log(err);
       return apiResponse.ErrorResponse(res, err.message);
     }
-  }
-]
+  },
+];
