74e9cd55e7fd7f2dc3a43019e2eeafafc6295342
Ravilochan <ravilochan2000@gmail.com>
2021-09-24 16:53:10 +0530

    Dependecies updated

--- backend/employee_service/app.js
+++ backend/employee_service/app.js
@@ -8,35 +8,40 @@ const indexRouter = require("./routes/index");
 const apiRouter = require("./routes/api");
 const apiResponse = require("./helpers/apiResponse");
 const cors = require("cors");
-const swaggerUi = require('swagger-ui-express');
-const openApiDocumentation = require('./openApiDocumentation');
-// const {getFileStream} = require("./helpers/s3");
+const swaggerUi = require("swagger-ui-express");
+const openApiDocumentation = require("./openApiDocumentation");
 // DB connection
 const MONGODB_URL = process.env.MONGODB_URL;
 const mongoose = require("mongoose");
-mongoose.connect(MONGODB_URL, { useNewUrlParser: true ,useUnifiedTopology: true, useFindAndModify: false,useCreateIndex: true }).then(() => {
-	//don't show the log when it is test
-	if(process.env.NODE_ENV !== "test") {
-		console.log("Connected to %s", MONGODB_URL);
-		console.log("Employee Service is running ... \n");
-	}
-})
-	.catch(err => {
-		console.error("App starting error:", err.message);
-		process.exit(1);
-	});
+mongoose
+  .connect(MONGODB_URL, {
+    keepAlive: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    //don't show the log when it is test
+    if (process.env.NODE_ENV !== "test") {
+      console.log("Connected to %s", MONGODB_URL);
+      console.log("Employee Service is running ... \n");
+    }
+  })
+  .catch((err) => {
+    console.error("App starting error:", err.message);
+    process.exit(1);
+  });
 const db = mongoose.connection;
 
 var app = express();
 
 //don't show the log when it is test
-if(process.env.NODE_ENV !== "test") {
-	app.use(logger("dev"));
+if (process.env.NODE_ENV !== "test") {
+  app.use(logger("dev"));
 }
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openApiDocumentation));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(openApiDocumentation));
 
 //To allow cross-origin requests
 app.use(cors());
@@ -46,14 +51,14 @@ app.use("/", indexRouter);
 app.use("/usermanagement/api/", apiRouter);
 
 // throw 404 if URL not found
-app.all("*", function(req, res) {
-	return apiResponse.notFoundResponse(res, "Page not found");
+app.all("*", function (req, res) {
+  return apiResponse.notFoundResponse(res, "Page not found");
 });
 
 app.use((err, req, res) => {
-	if(err.name == "UnauthorizedError"){
-		return apiResponse.unauthorizedResponse(res, err.message);
-	}
+  if (err.name == "UnauthorizedError") {
+    return apiResponse.unauthorizedResponse(res, err.message);
+  }
 });
 
 module.exports = app;
--- backend/event_service/app.js
+++ backend/event_service/app.js
@@ -11,24 +11,30 @@ var cors = require("cors");
 // DB connection
 var MONGODB_URL = process.env.MONGODB_URL;
 var mongoose = require("mongoose");
-mongoose.connect(MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true , useCreateIndex:true}).then(() => {
-	//don't show the log when it is test
-	if(process.env.NODE_ENV !== "test") {
-		console.log("Connected to %s", MONGODB_URL);
-		console.log("Event Service is running ... \n");
-	}
-})
-	.catch(err => {
-		console.error("App starting error:", err.message);
-		process.exit(1);
-	});
+mongoose
+  .connect(MONGODB_URL, {
+    keepAlive: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    //don't show the log when it is test
+    if (process.env.NODE_ENV !== "test") {
+      console.log("Connected to %s", MONGODB_URL);
+      console.log("Event Service is running ... \n");
+    }
+  })
+  .catch((err) => {
+    console.error("App starting error:", err.message);
+    process.exit(1);
+  });
 var db = mongoose.connection;
 
 var app = express();
 
 //don't show the log when it is test
-if(process.env.NODE_ENV !== "test") {
-	app.use(logger("dev"));
+if (process.env.NODE_ENV !== "test") {
+  app.use(logger("dev"));
 }
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -42,14 +48,14 @@ app.use("/", indexRouter);
 app.use("/eventmanagement/api/", apiRouter);
 
 // throw 404 if URL not found
-app.all("*", function(req, res) {
-	return apiResponse.notFoundResponse(res, "API not found");
+app.all("*", function (req, res) {
+  return apiResponse.notFoundResponse(res, "API not found");
 });
 
 app.use((err, req, res) => {
-	if(err.name == "UnauthorizedError"){
-		return apiResponse.unauthorizedResponse(res, err.message);
-	}
+  if (err.name == "UnauthorizedError") {
+    return apiResponse.unauthorizedResponse(res, err.message);
+  }
 });
 
 module.exports = app;
