94d8985b52723b739cc066d32aecc0a7e79864f8
prashanthb56 <prashanthb566@gmail.com>
2021-09-27 13:27:01 +0530

    user manage issues fixes

--- frontend/src/components/users/index.js
+++ frontend/src/components/users/index.js
@@ -90,7 +90,7 @@ const Users = (props) => {
       </div>
       <div className="full-width">
         <div className="filter">
-          <div className="row" style={{ flexBasis: '95%' }}>
+          <div className="row" style={{ flexBasis: '95%',margin:"unset" }}>
             <span className="col box headerText">Name</span>
 
             <div class="vl text-center"></div>
