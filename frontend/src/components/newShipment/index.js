1cce44b62d50fe1380c1cac7b603ccdcf18d28d3
Maheshkumar G <Maheshkumarg@apples-MacBook-Pro.local>
2021-10-05 14:31:26 +0530

    Bug fix for create shipment

--- frontend/src/components/newShipment/index.js
+++ frontend/src/components/newShipment/index.js
@@ -298,8 +298,11 @@ console.log(user.organisation)
       shipmentID,
       products,
     } = values;
+    let errorMsg = '';
     products.forEach((p) => {
-      if (p.productQuantity < 1) error = true;
+      if (p.productQuantity < 1) { error = true; errorMsg = 'product quantity'; }
+      else if (!p.batchNumber) {error = true; errorMsg = 'batch number';
+    }
     });
     if (!error) {
       const data = {
@@ -357,12 +360,11 @@ console.log(user.organisation)
         console.log("product quantity is undefined ");
         setShipmentError("Check product quantity");
         setOpenShipmentFail(true);
-      } else {
-        /* else if(check===2)
+      } else if(check===2)
       {
         setShipmentError("Check Batch Number");
         setOpenShipmentFail(true);
-      } */
+      } else {
         let i, j;
         let nn = data.products.length;
         for (i = 0; i < data.products.length; i++) {
@@ -400,13 +402,15 @@ console.log(user.organisation)
               type: "Success",
             });
           } else {
+
+            setShipmentError(result.data.message);
             setOpenShipmentFail(true);
             setErrorMessage("Create Shipment Failed");
           }
         }
       }
     } else {
-      setShipmentError("Check product quantity");
+      setShipmentError("Check "+errorMsg);
       setOpenShipmentFail(true);
     }
   };
@@ -671,8 +675,6 @@ console.log(user.organisation)
                             setAddProducts((p) => []);
                             setOrderIdSelected(true);
                             setFieldValue("OrderId", v.value);
-                            console.log(v.value, "v.value");
-                            
                             setOrderId(v.value);
                             dispatch(turnOn());
                             let result = await dispatch(getOrder(v.value));
