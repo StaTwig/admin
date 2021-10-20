a8a03b7696ed410fe38277dd4e7dab4fa4a54be0
imarjun <imarjunmuthusamy@gmail.com>
2021-10-20 16:05:05 +0530

    batch expiry show btn & notification icon align

--- frontend/src/components/productinventory/index.js
+++ frontend/src/components/productinventory/index.js
@@ -60,7 +60,7 @@ const ProductInventory = (props) => {
   const ref = useRef(null);
 
   const scrollCheck = () => {
-    console.log(ref.current.scrollLeft)
+    console.log(ref.current.scrollLeft);
     setscrollX(ref.current.scrollLeft);
     if (
       Math.floor(ref.current.scrollWidth - ref.current.scrollLeft) <=
@@ -73,7 +73,7 @@ const ProductInventory = (props) => {
   };
 
   const scroll = (shift) => {
-    console.log(ref.current)
+    console.log(ref.current);
     ref.current.scrollLeft += shift;
     setscrollX(scrollX + shift);
 
@@ -88,23 +88,23 @@ const ProductInventory = (props) => {
   };
   // setData(inventories.filter(r => r.payloadData.data.products[0].type == cat));
   return (
-    <div className='productinventory'>
-      <div className='d-flex justify-content-between'>
-        <h1 className='breadcrumb'>
+    <div className="productinventory">
+      <div className="d-flex justify-content-between">
+        <h1 className="breadcrumb">
           {enable ? "PRODUCT CATEGORY" : "PRODUCTS OUT OF STOCK"}
         </h1>
-        <div className='d-flex'>
-          <Link to='/addNewCategory'>
+        <div className="d-flex">
+          <Link to="/addNewCategory">
             <button
-              className='btn btn-yellow mr-3'
+              className="btn btn-yellow mr-3"
               style={{ position: "relative", top: "-15px" }}
             >
               <img
                 src={Add}
-                width='13'
-                height='13'
-                className='mr-2 mb-1'
-                alt='Add'
+                width="13"
+                height="13"
+                className="mr-2 mb-1"
+                alt="Add"
               />
               <span>
                 <b>Add New Category</b>
@@ -114,73 +114,89 @@ const ProductInventory = (props) => {
         </div>
       </div>
       {enable && (
-        <div className='main'>
-          <div onScroll={scrollCheck} style={{overflowX: 'scroll'}} className='row ml-0 flex-nowrap' ref={ref}>
+        <div className="main">
+          <div
+            onScroll={scrollCheck}
+            style={{ overflowX: "scroll" }}
+            className="row ml-0 flex-nowrap"
+            ref={ref}
+          >
             {categoryArray.map((cat) => (
               <div
                 className={`panel m-2 ${category === cat && `active`}`}
                 onClick={() => changeType(cat)}
               >
-                <div className='flex flex-column'>
+                <div className="flex flex-column">
                   <div className="picture">
-                    <img src={category === cat
-                        ? TotalInventoryAdded
-                        : ProductSelected
-                    } alt='truck'/>
+                    <img
+                      src={
+                        category === cat ? TotalInventoryAdded : ProductSelected
+                      }
+                      alt="truck"
+                    />
                   </div>
-                  <div className={`pt-3 flex text-dark font-weight-bold ${category === cat || `text-muted`}`}>{cat}</div>
+                  <div
+                    className={`pt-3 flex text-dark font-weight-bold ${
+                      category === cat || `text-muted`
+                    }`}
+                  >
+                    {cat}
+                  </div>
                 </div>
               </div>
             ))}
-            
           </div>
-          <button className="toggle-button-right" onClick={() => scroll(+100)}><img src={Next} className="toggle-icon-next" alt='truck'/></button>
-          <button className="toggle-button-left" onClick={() => scroll(-100)}><img src={Next} className="toggle-icon-back" alt='truck'/></button>
+          <button className="toggle-button-right" onClick={() => scroll(+100)}>
+            <img src={Next} className="toggle-icon-next" alt="truck" />
+          </button>
+          <button className="toggle-button-left" onClick={() => scroll(-100)}>
+            <img src={Next} className="toggle-icon-back" alt="truck" />
+          </button>
         </div>
       )}
-      <div className='row'>
-        <div className='p-2 mt-3 rounded full-width-ribbon'>
-          <div className='row filter'>
-            <div className='col-3'>
-              <img src={Product} width='16' height='16' alt='Product Name' />
-              <span className='ml-2 font-small'>Product Name</span>
+      <div className="row">
+        <div className="p-2 mt-3 rounded full-width-ribbon">
+          <div className="row filter">
+            <div className="col-3">
+              <img src={Product} width="16" height="16" alt="Product Name" />
+              <span className="ml-2 font-small">Product Name</span>
             </div>
-            <div className='col-3'>
+            <div className="col-3">
               <img
                 src={Quantity}
-                width='25'
-                height='16'
-                alt='Product Category'
+                width="25"
+                height="16"
+                alt="Product Category"
               />
-              <span className='ml-2 font-small'>Product Category</span>
+              <span className="ml-2 font-small">Product Category</span>
             </div>
-            <div className='col-3'>
-              <img src={user} width='16' height='16' alt='Manufacturer' />
-              <span className='ml-2 font-small'>Manufacturer</span>
+            <div className="col-3">
+              <img src={user} width="16" height="16" alt="Manufacturer" />
+              <span className="ml-2 font-small">Manufacturer</span>
             </div>
-            <div className='col-2'>
-              <img src={Quantity} width='25' height='16' alt='Quantity' />
-              <span className='ml-2 font-small'>Quantity</span>
+            <div className="col-2">
+              <img src={Quantity} width="25" height="16" alt="Quantity" />
+              <span className="ml-2 font-small">Quantity</span>
             </div>
           </div>
         </div>
         {console.log(data)}
-        <div className='ribbon-space col'>
+        <div className="ribbon-space col">
           {data.map(
             (inv, i) => (
-              <div key={i} className='col mb-3 rounded row bg-white shadow'>
-                <div className='col-3 txt '>
+              <div key={i} className="col mb-3 rounded row bg-white shadow">
+                <div className="col-3 txt ">
                   {inv.products.name ? inv.products.name : "N/A"}
                 </div>
-                <div className='col-3 txt1'>
+                <div className="col-3 txt1">
                   {inv.products.type ? inv.products.type : "N/A"}
                 </div>
-                <div className='col-2 txt1'>
+                <div className="col-2 txt1">
                   {inv.products.manufacturer
                     ? inv.products.manufacturer
                     : "N/A"}
                 </div>
-                <div className='col-2 txt1 text-right'>
+                <div className="col-2 txt1 text-right">
                   {inv.inventoryDetails.quantity
                     ? inv.inventoryDetails.quantity
                     : "0"}
@@ -190,13 +206,13 @@ const ProductInventory = (props) => {
                     : "N/A"}
                   {")"}
                 </div>
-                <div className='col'>
+                <div className="col">
                   <button
-                    type='button'
+                    type="button"
                     onClick={() =>
                       props.history.push(`/viewproduct`, { data: inv })
                     }
-                    className='bttn1-blue blue-primary'
+                    className="bttn1-blue blue-primary"
                   >
                     Show more
                   </button>
@@ -206,8 +222,8 @@ const ProductInventory = (props) => {
             // </div>
           )}
           {data?.length === 0 && (
-            <div className='col-12 p-3 mb-3 rounded row bg-white shadow'>
-              <div className='col-12 txt text-center txtBlue'>
+            <div className="col-12 p-3 mb-3 rounded row bg-white shadow">
+              <div className="col-12 txt text-center txtBlue">
                 No records found
               </div>
             </div>
