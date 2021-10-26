b68d84a99176e73bac8fb3ab70dab45dd6727c4e
Ubuntu <ubuntu@ip-172-31-0-71.ap-south-1.compute.internal>
2021-10-26 03:00:01 +0000

    ABINBEV PROD FILES 26/10/2021

--- AbInBev/frontend/src/config.js
+++ AbInBev/frontend/src/config.js
@@ -187,7 +187,7 @@ export function config() {
     },
   };
 
-  const environment = process.env.ENVIRONMENT || 'ainbevtest'; // change prod to test, local,stable, dev for respective environments
+  const environment = process.env.ENVIRONMENT || 'ainbevprod'; // change prod to test, local,stable, dev for respective environments
   const conf = confs[environment];
   return conf;
 }
