6cbf3ea068634dd844039d3e2a5af1df905e16fe
Ravilochan <ravilochan2000@gmail.com>
2021-11-16 16:53:47 +0530

    Image & Receive Shipment Fix

--- backend/last_mile_service/routes/lastmile.js
+++ backend/last_mile_service/routes/lastmile.js
@@ -1,30 +1,30 @@
 const express = require("express");
-const multer = require("multer");
 const LastMileController = require("../controllers/LastMileController");
-
-const Storage = multer.diskStorage({
-  destination(req, file, callback) {
-    callback(null, "./uploads");
-  },
-  filename(req, file, callback) {
-    callback(null, file.originalname);
-  },
-});
-
-const upload = multer({ storage: Storage });
-
 const router = express.Router();
 
-
-router.get("/GetEOLInfoBySerialNumber", LastMileController.getEOLInfoBySerialNumber);
+router.get(
+  "/GetEOLInfoBySerialNumber",
+  LastMileController.getEOLInfoBySerialNumber
+);
 router.get("/GetEOLInfoByProductId", LastMileController.GetEOLInfoByProductId);
-router.get("/GetEOLInfoByIdentityId", LastMileController.GetEOLInfoByIdentityId);
-router.get("/GetEOLInfoByPlaceAdministered", LastMileController.GetEOLInfoByPlaceAdministered);
-router.get("/GetEOLListByDateWindow", LastMileController.GetEOLListByDateWindow);
+router.get(
+  "/GetEOLInfoByIdentityId",
+  LastMileController.GetEOLInfoByIdentityId
+);
+router.get(
+  "/GetEOLInfoByPlaceAdministered",
+  LastMileController.GetEOLInfoByPlaceAdministered
+);
+router.get(
+  "/GetEOLListByDateWindow",
+  LastMileController.GetEOLListByDateWindow
+);
 router.post("/AddNewEOL", LastMileController.AddNewEOL);
 router.post("/UpdateExistingEOLByID", LastMileController.UpdateExistingEOLByID);
 router.get("/GetEOLInfo", LastMileController.getEOLInfo);
-router.get("/getProductsByWarehouse", LastMileController.getProductsByWarehouse);
-
+router.get(
+  "/getProductsByWarehouse",
+  LastMileController.getProductsByWarehouse
+);
 
 module.exports = router;
