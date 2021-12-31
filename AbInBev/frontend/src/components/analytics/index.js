7b04aa598e323b6e6909d694e24ecbbaf3c2c39a
Saravanan Jodavula <jvs.ravanan@gmail.com>
2021-12-31 18:59:18 +0530

    targets bugs fixed

--- AbInBev/frontend/src/components/analytics/index.js
+++ AbInBev/frontend/src/components/analytics/index.js
@@ -63,10 +63,10 @@ const Analytics = (props) => {
   const [isActive, setIsActive] = useState('by_yearly');
   const [Otype, setOtype] = useState('ALL_VENDORS');
   const [selectedViewCode, setSelectedViewCode] = useState(
-    'SPM_DASHBOARD',
+    'ANNUALREPORT_DASHBOARD',
   );
   const [selectedModule, setSelectedModule] = useState(
-    'SPM_DASHBOARD',
+    'ANNUALREPORT_DASHBOARD',
   );
   const [annualReportButton, setannualReportButton] = useState('btn active');
   const [inventoryButton, setInventoryButton] = useState('btn');
