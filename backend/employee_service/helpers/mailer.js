ded0398dcd4cf40ca5f5b9e4e16f73f68d7adbc3
Ravilochan <ravilochan2000@gmail.com>
2022-01-05 10:38:55 +0530

    Fixing Employee Service Errors

--- backend/employee_service/helpers/mailer.js
+++ backend/employee_service/helpers/mailer.js
@@ -1,9 +1,8 @@
-const nodemailer = require('nodemailer');
+const nodemailer = require("nodemailer");
 
-// create reusable transporter object using the default SMTP transport
-let transporter = nodemailer.createTransport({
-  host: 'smtp.gmail.com',
-  port: '587',
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: "587",
   //secure: process.env.EMAIL_SMTP_SECURE, // lack of ssl commented this. You can uncomment it.
   auth: {
     user: `${process.env.EMAIL_SMTP_USERNAME}`,
@@ -14,35 +13,31 @@ let transporter = nodemailer.createTransport({
   },
 });
 
-exports.send = function(from, to, subject, html) {
-  // send mail with defined transport object
-  // visit https://nodemailer.com/ for more options
+exports.send = function (from, to, subject, html) {
   return transporter.sendMail({
-    from: from, // sender address e.g. no-reply@xyz.com or "Fred Foo 👻" <foo@example.com>
-    to: to, // list of receivers e.g. bar@example.com, baz@example.com
-    subject: subject, // Subject line e.g. 'Hello ✔'
-    //text: text, // plain text body e.g. Hello world?
-    html: html, // html body e.g. '<b>Hello world?</b>'
+    from: from,
+    to: to,
+    subject: subject,
+    //text: text,
+    html: html,
   });
 };
 
-exports.validateEmail = email => {
-  var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+exports.validateEmail = (email) => {
+  const re =
+    /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   if (re.test(email)) {
-    //Email valid. Procees to test if it's from the right domain (Second argument is to check that the string ENDS with this domain, and that it doesn't just contain it)
     if (
+      email.indexOf("@statwig.com", email.length - "@statwig.com".length) !==
+        -1 ||
       email.indexOf(
-        '@statwig.com',
-        email.length - '@statwig.com'.length,
-      ) !== -1 || email.indexOf(
-        '@cloudleaf.com',
-        email.length - '@cloudleaf.com'.length,
+        "@cloudleaf.com",
+        email.length - "@cloudleaf.com".length
       ) !== -1
     ) {
-      //VALID
       return true;
     }
+    return true;
   }
-  //return false;
-  return true;//Removing email validation check
+  return false;
 };
