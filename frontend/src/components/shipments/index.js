322e233a7957acf5fbb4ddb519448b7c331223f3
Himabindu <himabindu13011998@gmail.com>
2022-02-11 12:57:38 +0530

    export as mail option was removed

--- frontend/src/components/shipments/index.js
+++ frontend/src/components/shipments/index.js
@@ -349,7 +349,7 @@ const ShipmentAnalytic = (props) => {
     setExportFilterData([
       { key: "excel", value: "excel", label: t("excel"), checked: false },
       { key: "pdf",   value: "pdf", label: t("pdf"), checked: false },
-      { key: "email", value: "mail", label: t("mail"), checked: false },
+      // { key: "email", value: "mail", label: t("mail"), checked: false },
       // { key: "print", value: "Print", checked: false },
     ]);
   }, []);
