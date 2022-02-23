0f416a2b7b5e8407a89dbe538223c56f7575be8a
Siddhant Parekh <parekhsiddhant@gmail.com>
2022-02-23 16:24:01 +0530

    Mfg & Exp date now visible

--- backend/shipment_service/controllers/ShipmentController.js
+++ backend/shipment_service/controllers/ShipmentController.js
@@ -1834,6 +1834,12 @@ exports.viewShipment = [
               id: element.productID,
             });
             element.unitofMeasure = product.unitofMeasure;
+
+            const batch = await AtomModel.findOne({
+              batchNumbers: element.batchNumber,
+            });
+            element.mfgDate = batch?.attributeSet.mfgDate;
+            element.expDate = batch?.attributeSet.expDate;
           });
           return apiResponse.successResponseWithData(
             res,
