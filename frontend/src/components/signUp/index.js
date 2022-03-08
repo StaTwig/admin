693cd0731554e35e8ab6a3f1468a798b156b532b
Sumit Kumar Sharma <ss8559283@gmail.com>
2022-03-08 15:17:36 +0530

    onenter when focus on option other of orgname rise popup

--- frontend/src/components/signUp/index.js
+++ frontend/src/components/signUp/index.js
@@ -88,7 +88,7 @@ const FormPage = (props) => {
       } else {
         setEmailErrorMsg("");
         verifyEmailAndPhoneNo(`emailId=${props.email}`).then((v) => {
-          console.log("v", v);
+          // console.log("v", v);
           if (v.data.length) {
             setemailerror(true);
             setsignupDisable(true);
@@ -157,6 +157,62 @@ const FormPage = (props) => {
   ) {
     setsignupDisable(false);
     setChecker(false);
+  }
+
+
+  const handleOrgName = (event, item, setFieldValue) => {
+    console.log("Entered org name")
+    if (firstName.length <= 0) {
+      setFirstNameError(true);
+    }
+    if (lastName.length <= 0) {
+      setLastNameError(true);
+    }
+    if (
+      (mobileNumber.length <= 0 ||
+        email.length <= 0) &&
+      emailErrorMsg === "" &&
+      phoneErrorMsg === "" &&
+      (emailError || !phoneNumberError) &&
+      (!emailError || phoneNumberError)
+    ) {
+      setPhoneNumberError(true);
+      setMailError(true);
+    }
+    setFieldValue("org", item);
+    props.onOrganisationChange(item);
+    if (item.name !== t("other")) {
+      setValue(item.name);
+      props.onOrgChange(false);
+    }
+    if (item.name === t("other")) {
+      props.onOrgChange(true);
+
+      if (
+        firstName.length > 0 &&
+        lastName.length > 0 &&
+        orgType.length > 0 &&
+        (email.length > 0 ||
+          mobileNumber.length > 0) &&
+        emailErrorMsg === "" &&
+        phoneErrorMsg === ""
+        // &&
+        // (emailError || !phoneNumberError) && (!emailError || phoneNumberError)
+      ) {
+        props.onOrgChange(true);
+      } else {
+        setPhoneNumberError(true);
+        setMailError(true);
+        props.onOrgChange(false);
+      }
+    }
+  }
+
+  const unWantedSubmit = (keyEvent) => {
+   
+    if ((keyEvent.charCode || keyEvent.keyCode) === 13 && keyEvent.keyCode !== 9) {
+      keyEvent.stopPropagation();
+    }
   }
 
   return (
@@ -242,9 +298,7 @@ const FormPage = (props) => {
                     }) => (
                       <form
                         onSubmit={handleSubmit}
-                        onKeyDown={(e) =>
-                          e.keyCode === 13 && e.preventDefault()
-                        }
+                        onKeyDown={unWantedSubmit}
                         className="mb-5"
                       >
                         <div className="login-form mt-1 pl-5 pr-5 ml-5">
@@ -641,52 +695,7 @@ const FormPage = (props) => {
                               }}
                             >
                               <Autocomplete
-                                onChange={(event, item) => {
-                                  if (firstName.length <= 0) {
-                                    setFirstNameError(true);
-                                  }
-                                  if (lastName.length <= 0) {
-                                    setLastNameError(true);
-                                  }
-                                  if (
-                                    (mobileNumber.length <= 0 ||
-                                      email.length <= 0) &&
-                                    emailErrorMsg === "" &&
-                                    phoneErrorMsg === "" &&
-                                    (emailError || !phoneNumberError) &&
-                                    (!emailError || phoneNumberError)
-                                  ) {
-                                    setPhoneNumberError(true);
-                                    setMailError(true);
-                                  }
-                                  setFieldValue("org", item);
-                                  props.onOrganisationChange(item);
-                                  if (item.name !== t("other")) {
-                                    setValue(item.name);
-                                    props.onOrgChange(false);
-                                  }
-                                  if (item.name === t("other")) {
-                                    props.onOrgChange(true);
-
-                                    if (
-                                      firstName.length > 0 &&
-                                      lastName.length > 0 &&
-                                      orgType.length > 0 &&
-                                      (email.length > 0 ||
-                                        mobileNumber.length > 0) &&
-                                      emailErrorMsg === "" &&
-                                      phoneErrorMsg === ""
-                                      // &&
-                                      // (emailError || !phoneNumberError) && (!emailError || phoneNumberError)
-                                    ) {
-                                      props.onOrgChange(true);
-                                    } else {
-                                      setPhoneNumberError(true);
-                                      setMailError(true);
-                                      props.onOrgChange(false);
-                                    }
-                                  }
-                                }}
+                                onChange={(event, item) => handleOrgName(event, item, setFieldValue)}
                                 id="debug"
                                 debug
                                 getOptionLabel={(option) => option.name}
@@ -776,7 +785,7 @@ const FormPage = (props) => {
                               </Alert>
                             </div>
                           )}
-                          {console.log(props.errorMessage)}
+                          {/* {console.log(props.errorMessage)} */}
 
                           <div className="text-center">
                             <br></br>
