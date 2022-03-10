6d2daed76f99eba151a6f10fae0f4391e12915b8
Siddhant Parekh <parekhsiddhant@gmail.com>
2022-03-10 10:10:27 +0000

    Update index.js

--- frontend/src/components/orders/ExcelPopup/index.js
+++ frontend/src/components/orders/ExcelPopup/index.js
@@ -34,14 +34,11 @@ const ExcelPopUp = (props) => {
     formData.append("excel", excel);
     dispatch(turnOn());
     const result = await addPOsFromExcel(formData);
-    console.log("Result - ", result);
     if (result && result.status === 200) {
       let arr = result.data.data.inserted;
-      console.log("Inserted records - ", result.data.data.inserted);
       let notNullValues = 0;
       if (arr && arr.length > 0)
         for (let i = 0; i < arr.length; i++) {
-          console.log(arr[i]);
           if (arr[i] != null) {
             notNullValues++;
           }
