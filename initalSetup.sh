3261ced4812b845c93b87be35088676299e767ab
Ravilochan <ravilochan2000@gmail.com>
2022-04-05 08:50:28 +0530

    AB-demo with docker images in gitlab

--- initalSetup.sh
+++ initalSetup.sh
@@ -13,7 +13,7 @@ sudo chmod +x /usr/local/bin/docker-compose
 docker-compose --version
 
 # sudo curl -L --fail https://raw.githubusercontent.com/linuxserver/docker-docker-compose/master/run.sh -o /usr/local/bin/docker-compose
-# sudo chmod + x /usr/local/bin/docker-compose
+# sudo chmod +x /usr/local/bin/docker-compose
 
 sudo apt-get install build-essential -y
 
