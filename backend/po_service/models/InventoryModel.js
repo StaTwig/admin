fe473fcf881d7b13807580db3a4a97393eaa846b
Ravilochan <ravilochan2000@gmail.com>
2022-07-20 08:20:06 +0530

    inventory Model sync

--- backend/po_service/models/InventoryModel.js
+++ backend/po_service/models/InventoryModel.js
@@ -1,15 +1,24 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const InventorySchema = new mongoose.Schema(
   {
     id: { type: String, required: true, unique: true },
-    inventory_details: {
-      type: Array,
-      default: [
-        
-      ],
-    },
+    inventoryDetails: [
+      {
+        type: new mongoose.Schema(
+          {
+            productId: { type: String, required: true },
+            quantity: { type: Number, default: 0 },
+            quantityInTransit: { type: Number, default: 0 },
+            totalSales: { type: Number, default: 0 },
+          },
+          {
+            timestamps: true,
+          }
+        ),
+      },
+    ],
   },
-  { timestamps: true },
+  { timestamps: true }
 );
-module.exports = mongoose.model('Inventory', InventorySchema);
+module.exports = mongoose.model("Inventory", InventorySchema);
--- backend/po_service/models/InventoryModel_Old.js
+++ backend/po_service/models/InventoryModel_Old.js
@@ -1,14 +1,14 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const InventorySchema = new mongoose.Schema(
   {
     manufacturingDate: { type: String },
     expiryDate: { type: String },
-    
+
     owner: { type: String },
     transactionIds: { type: Array },
     productName: { type: String },
   },
-  { timestamps: true },
+  { timestamps: true }
 );
-module.exports = mongoose.model('Inventory', InventorySchema);
+module.exports = mongoose.model("Inventory", InventorySchema);
