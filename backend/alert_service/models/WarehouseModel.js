229fa4f94c0956e34c07fb6e2b4e19362084aaaf
Ravilochan <ravilochan2000@gmail.com>
2022-07-25 13:23:36 +0530

    warehouse model updated

--- backend/alert_service/models/WarehouseModel.js
+++ backend/alert_service/models/WarehouseModel.js
@@ -2,40 +2,45 @@ const mongoose = require("mongoose");
 const WarehouseSchema = new mongoose.Schema(
   {
     id: { type: String, required: true, unique: true },
+    title: { type: String, required: true, default: "Warehouse" },
     organisationId: {
       type: String,
       required: true,
-      default: "org123",
     },
     postalAddress: {
       type: String,
-      required: false,
       default: null,
     },
-    region: {
+    warehouseAddress: {
       type: Object,
       required: true,
-      default: {
-        regionId: "reg123",
-        regionName: "Earth Prime",
+      properties: {
+        firstLine: { type: String },
+        secondLine: { type: String },
+        city: { type: String },
+        state: { type: String },
+        country: { type: String },
+        landmark: { type: String },
+        zipCode: { type: String },
       },
     },
+    region: { type: String, required: true },
     country: {
-      type: Object,
+      type: String,
       required: true,
-      default: {
-        countryId: "001",
-        countryName: "India",
-      },
     },
     location: {
-      type: Object,
-      default: {
-        longitude: 12.12323453534,
-        latitude: 13.123435345435,
-        geohash: "1231nejf923453",
+      type: {
+        type: String,
+        enum: ["Point"],
+        default: "Point",
       },
+      coordinates: {
+        type: [Number],
+      },
+      geohash: String,
     },
+    status: String,
     supervisors: {
       type: Array,
       default: [],
@@ -48,6 +53,16 @@ const WarehouseSchema = new mongoose.Schema(
       type: String,
       required: true,
       unique: true,
+    },
+    bottleCapacity: {
+      type: Number,
+      min: 0,
+      default: 0,
+    },
+    sqft: {
+      type: Number,
+      min: 0,
+      default: 0,
     },
   },
   { timestamps: true }
--- backend/analytics_service/models/WarehouseModel.js
+++ backend/analytics_service/models/WarehouseModel.js
@@ -1,55 +1,70 @@
-var mongoose = require('mongoose');
-var WarehouseSchema = new mongoose.Schema(
+const mongoose = require("mongoose");
+const WarehouseSchema = new mongoose.Schema(
   {
     id: { type: String, required: true, unique: true },
+    title: { type: String, required: true, default: "Warehouse" },
     organisationId: {
       type: String,
       required: true,
-      default: 'org123',
     },
     postalAddress: {
       type: String,
-      required: false,
       default: null,
     },
-    region: {
+    warehouseAddress: {
       type: Object,
       required: true,
-      default: {
-        regionId: 'reg123',
-        regionName: 'Earth Prime',
+      properties: {
+        firstLine: { type: String },
+        secondLine: { type: String },
+        city: { type: String },
+        state: { type: String },
+        country: { type: String },
+        landmark: { type: String },
+        zipCode: { type: String },
       },
     },
+    region: { type: String, required: true },
     country: {
-      type: Object,
+      type: String,
       required: true,
-      default: {
-        countryId: '001',
-        countryName: 'India',
-      },
     },
     location: {
-      type: Object,
-      default: {
-        longitude: 12.12323453534,
-        latitude: 13.123435345435,
-        geohash: '1231nejf923453',
+      type: {
+        type: String,
+        enum: ["Point"],
+        default: "Point",
       },
+      coordinates: {
+        type: [Number],
+      },
+      geohash: String,
     },
+    status: String,
     supervisors: {
       type: Array,
-      default: []
+      default: [],
     },
     employees: {
       type: Array,
-      default: []
+      default: [],
     },
-   warehouseInventory: {
+    warehouseInventory: {
       type: String,
-     required: true,
-     unique: true
-   },
+      required: true,
+      unique: true,
+    },
+    bottleCapacity: {
+      type: Number,
+      min: 0,
+      default: 0,
+    },
+    sqft: {
+      type: Number,
+      min: 0,
+      default: 0,
+    },
   },
-  { timestamps: true },
+  { timestamps: true }
 );
-module.exports = mongoose.model('Warehouse', WarehouseSchema);
+module.exports = mongoose.model("Warehouse", WarehouseSchema);
