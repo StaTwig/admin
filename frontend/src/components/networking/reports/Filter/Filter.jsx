9c5ef113fcff09839e1972c23c5fe7813ffc06d0
Ravilochan <ravilochan2000@gmail.com>
2022-08-08 16:47:01 +0530

    class - Warning

--- frontend/src/components/networking/reports/Filter/Filter.jsx
+++ frontend/src/components/networking/reports/Filter/Filter.jsx
@@ -26,15 +26,15 @@ export default function Filter({ title }) {
           className={className}
           aria-labelledby={labeledBy}
         >
-          <div className="form-space-area">
+          <div className='form-space-area'>
             <Form.Control
               autoFocus
-              placeholder="Type to filter..."
+              placeholder='Type to filter...'
               onChange={(e) => setValue(e.target.value)}
               value={value}
             />
           </div>
-          <ul className="list-unstyled mi-custom-list-height">
+          <ul className='list-unstyled mi-custom-list-height'>
             {React.Children.toArray(children).filter(
               (child) =>
                 !value || child.props.children.toLowerCase().startsWith(value)
@@ -46,27 +46,27 @@ export default function Filter({ title }) {
   );
   return (
     <Dropdown>
-      <Dropdown.Toggle as={CustomToggle} id="dropdown-custom-components">
-        <div className="table-header-with-filter">
-          <p className="mi-body-sm mi-reset grey-400">{title}</p>
-          <i class="fa-solid fa-sort"></i>
+      <Dropdown.Toggle as={CustomToggle} id='dropdown-custom-components'>
+        <div className='table-header-with-filter'>
+          <p className='mi-body-sm mi-reset grey-400'>{title}</p>
+          <i className='fa-solid fa-sort'></i>
         </div>
       </Dropdown.Toggle>
 
-      <Dropdown.Menu as={CustomMenu} className="mi-custom-filter">
-        <Dropdown.Item eventKey="1">Red Data Aplication</Dropdown.Item>
-        <Dropdown.Item eventKey="2">Blue</Dropdown.Item>
-        <Dropdown.Item eventKey="3">Orange</Dropdown.Item>
-        <Dropdown.Item eventKey="1">Red-Orange</Dropdown.Item>
-        <Dropdown.Item eventKey="1">Red</Dropdown.Item>
-        <Dropdown.Item eventKey="2">Blue</Dropdown.Item>
-        <Dropdown.Item eventKey="3">Orange</Dropdown.Item>
-        <Dropdown.Item eventKey="1">Red</Dropdown.Item>
-        <Dropdown.Item eventKey="2">Blue</Dropdown.Item>
-        <Dropdown.Item eventKey="3">Orange</Dropdown.Item>
-        <Dropdown.Item eventKey="1">Red</Dropdown.Item>
-        <Dropdown.Item eventKey="2">Blue</Dropdown.Item>
-        <Dropdown.Item eventKey="3">Orange</Dropdown.Item>
+      <Dropdown.Menu as={CustomMenu} className='mi-custom-filter'>
+        <Dropdown.Item eventKey='1'>Red Data Aplication</Dropdown.Item>
+        <Dropdown.Item eventKey='2'>Blue</Dropdown.Item>
+        <Dropdown.Item eventKey='3'>Orange</Dropdown.Item>
+        <Dropdown.Item eventKey='1'>Red-Orange</Dropdown.Item>
+        <Dropdown.Item eventKey='1'>Red</Dropdown.Item>
+        <Dropdown.Item eventKey='2'>Blue</Dropdown.Item>
+        <Dropdown.Item eventKey='3'>Orange</Dropdown.Item>
+        <Dropdown.Item eventKey='1'>Red</Dropdown.Item>
+        <Dropdown.Item eventKey='2'>Blue</Dropdown.Item>
+        <Dropdown.Item eventKey='3'>Orange</Dropdown.Item>
+        <Dropdown.Item eventKey='1'>Red</Dropdown.Item>
+        <Dropdown.Item eventKey='2'>Blue</Dropdown.Item>
+        <Dropdown.Item eventKey='3'>Orange</Dropdown.Item>
       </Dropdown.Menu>
     </Dropdown>
   );
