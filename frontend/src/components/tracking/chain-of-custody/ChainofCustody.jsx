3a4fb7248c0dc0ca64881d1e9bb5740f6bb8e67e
Siddhant Parekh <parekhsiddhant@gmail.com>
2022-08-11 10:15:08 +0530

    Po data visible if present

--- frontend/src/components/tracking/chain-of-custody/ChainofCustody.jsx
+++ frontend/src/components/tracking/chain-of-custody/ChainofCustody.jsx
@@ -1,11 +1,23 @@
 import React from "react";
 import ChainGroup from "./ChainGroup";
+import PoCard from "./PoCard";
 import "./ChainofCustody.scss";
 import TrackIllustration from "../../../assets/images/track.webp";
 
 export default function ChainofCustody({ trackingData }) {
+	const poDetails = trackingData?.poDetails;
+
 	return trackingData ? (
 		<div className="chain-of-custody-container">
+			{poDetails && (
+				<div
+					className="chain-level-1"
+					style={{ gridTemplateColumns: `0rem 1fr` }}
+				>
+					<div className="null-space"></div>
+					<PoCard poDetails={poDetails} />
+				</div>
+			)}
 			{trackingData?.trackedShipment?.map((element, index) => (
 				<div
 					key={index}
