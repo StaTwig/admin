1a758ce324d263f508c5afcebd4d567e0f155002
statwigglobal <76856887+statwigglobal@users.noreply.github.com>
2022-08-30 09:50:26 +0530

    Update organisationActions.js

--- frontend/src/actions/organisationActions.js
+++ frontend/src/actions/organisationActions.js
@@ -67,7 +67,7 @@ export const getRequestsPending = (skip = 0, limit = 5) => {
         payload: result.data,
       });
       dispatch(turnOff());
-      return result.data.data.length;
+      return result?.data?.data?.length;
     };
   } catch (e) {
     throw Error(e.message);
