9105116eb5588fe3bd7af3ed58306e66c6141c90
tushar <tbajaj@iitg.ac.in>
2022-10-20 09:20:00 +0530

    resolved bug for exact date not displayed in the current location tab

--- frontend/src/components/tracking/Tracking.jsx
+++ frontend/src/components/tracking/Tracking.jsx
@@ -12,101 +12,119 @@ import FailedPopUp from "../../shared/PopUp/failedPopUp";
 import { useTranslation } from "react-i18next";
 
 export default function Tracking() {
-	const { id } = useParams();
+  const { id } = useParams();
   const history = useHistory();
   const { t } = useTranslation();
 
-	const [LocationTab, setLocationTab] = useState("CHAIN");
-	const [trackingID, setTrackingID] = useState(id);
-	const [trackingData, setTrackingData] = useState();
-	const [errorInTrack, setErrorInTrack] = useState("");
-	const [openFailedPopup, setOpenFailedPopup] = useState(false);
-
-	const closeModalFailedPopUp = () => {
-		setOpenFailedPopup(false);
+  const closeModalFailedPopUp = () => {
+    setOpenFailedPopup(false);
     setErrorInTrack("");
     history.push(`/track`);
     setTrackingID("");
-	};
+  };
 
-	useEffect(async () => {
-		try {
-			if (id) {
-				let result = await getJourneyTrack(id);
-				if (result.status === 200) {
-					setErrorInTrack("");
-					setTrackingData(result.data.data);
-				} else {
-					throw new Error(result.message);
-				}
-			}
-		} catch (err) {
-			console.log(err.message);
-			setErrorInTrack(err.message);
-			setOpenFailedPopup(true);
-		}
-	}, [id]);
+  const [LocationTab, setLocationTab] = useState("CHAIN");
+  const [trackingID, setTrackingID] = useState(id);
+  const [trackingData, setTrackingData] = useState();
+  const [errorInTrack, setErrorInTrack] = useState("");
+  const [openFailedPopup, setOpenFailedPopup] = useState(false);
 
-	const handleSearch = async () => {
-		try {
-			history.push(`/track/${trackingID}`);
-			// let result = await getJourneyTrack(trackingID);
-			// if(result.status === 200) {
-			//   setTrackingData(result.data.data);
-			// } else {
-			//   throw new Error(result);
-			// }
-		} catch (err) {
-			console.log("Error while fetching track details - ", err.message);
-		}
-	};
+  useEffect(async () => {
+    try {
+      if (id) {
+        let result = await getJourneyTrack(id);
+        if (result.status === 200) {
+          setErrorInTrack("");
+          setTrackingData(result.data.data);
+        } else {
+          throw new Error(result.message);
+        }
+      }
+    } catch (err) {
+      console.log(err.message);
+      setErrorInTrack(err.message);
+      setOpenFailedPopup(true);
+    }
+  }, [id]);
 
-	const handleKeyPress = (event) => {
-		if (event.key === "Enter") {
-			handleSearch();
-		}
-	};
+  const handleKeyPress = (event) => {
+    if (event.key === "Enter") {
+      handleSearch();
+    }
+  };
 
-	return (
-		<div className="tracking-main-layout">
-			<div className="track-grid-container">
-				<div className="tracking-content-area">
-					<div className="tracking-header">
-						<h1 style={{ paddingBottom: "10px" }} className="vl-heading-bdr black f-700 mi-reset">
-							Track & Trace
-						</h1>
-						<div className="tracking-search-bar">
-							<div className="mi-flex-ac">
-								<input
-									type="search"
-									placeholder="Search by Tracking ID"
-									className="track-search"
-									onKeyUp={handleKeyPress}
+  const handleSearch = async () => {
+    try {
+      history.push(`/track/${trackingID}`);
+      // let result = await getJourneyTrack(trackingID);
+      // if(result.status === 200) {
+      //   setTrackingData(result.data.data);
+      // } else {
+      //   throw new Error(result);
+      // }
+    } catch (err) {
+      console.log("Error while fetching track details - ", err.message);
+    }
+  };
+
+  return (
+    <div className="tracking-main-layout">
+      <div className="track-grid-container">
+        <div className="tracking-content-area">
+          <div className="tracking-header">
+            <h1
+              style={{ paddingBottom: "10px" }}
+              className="vl-heading-bdr black f-700 mi-reset"
+            >
+              Track & Trace
+            </h1>
+            <div className="tracking-search-bar">
+              <div className="mi-flex-ac">
+                <input
+                  type="search"
+                  placeholder="Search by Tracking ID"
+                  className="track-search"
+                  onKeyUp={handleKeyPress}
                   onChange={(event) => setTrackingID(event.target.value)}
                   value={trackingID}
-								/>
-								<i className="bx bx-search search-track-icon" onClick={handleSearch}></i>
-							</div>
-						</div>
-					</div>
-					<div className="tab-buttons">
-						<Tab layout="button" LocationTab={LocationTab} setLocationTab={setLocationTab} />
-					</div>
-					{LocationTab === "CHAIN" && <ChainofCustody trackingData={trackingData} />}
-					{LocationTab === "LOCATION" && (
-						<CurrentLocation currentLocationData={trackingData?.currentLocationData} />
-					)}
-				</div>
-				<div className="tracking-map-area">
-					<TrackingMap LocationTab={LocationTab} trackingData={trackingData} />
-				</div>
-			</div>
+                />
+                <i
+                  className="bx bx-search search-track-icon"
+                  onClick={handleSearch}
+                ></i>
+              </div>
+            </div>
+          </div>
+          <div className="tab-buttons">
+            <Tab
+              layout="button"
+              LocationTab={LocationTab}
+              setLocationTab={setLocationTab}
+            />
+          </div>
+          {LocationTab === "CHAIN" && (
+            <ChainofCustody trackingData={trackingData} />
+          )}
+          {LocationTab === "LOCATION" && (
+            <CurrentLocation
+              currentLocationData={trackingData?.currentLocationData}
+            />
+          )}
+        </div>
+        <div className="tracking-map-area">
+          <TrackingMap LocationTab={LocationTab} trackingData={trackingData} />
+        </div>
+      </div>
 
-			{openFailedPopup && (
-				<Modal close={() => closeModalFailedPopUp()} size="modal-sm">
-					<FailedPopUp message={errorInTrack} onHide={closeModalFailedPopUp} t={t} />
-				</Modal>
-			)}
-		</div>
-	);
+      {openFailedPopup && (
+        <Modal close={() => closeModalFailedPopUp()} size="modal-sm">
+          <FailedPopUp
+            message={errorInTrack}
+            onHide={closeModalFailedPopUp}
+            t={t}
+          />
+        </Modal>
+      )}
+    </div>
+  );
 }
