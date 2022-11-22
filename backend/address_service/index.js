f8d47e206e513cd410c98c7fe0a7e7d722220e12
Saravanan Jodavula <jvs.ravanan@gmail.com>
2022-11-22 08:39:56 +0530

    dockerfiles change

--- backend/address_service/index.js
+++ backend/address_service/index.js
@@ -8,7 +8,7 @@ var http = require('http');
 /**
  * Get port from environment and store in Express.
  */
-var port = normalizePort(process.env.PORT || '3001');
+var port = normalizePort(process.env.PORT || '3019');
 app.set('port', port);
 
 /**
